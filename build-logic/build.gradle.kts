e7bf4633c87a21c43c000a70e694a0af76a2acfc
dltkd1395 <dltkd13956@naver.com>
2024-01-28 00:30:26 +0900

    chore: gradlePlugin 설정

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -3,3 +3,23 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     `kotlin-dsl`
 }
+
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(libs.android.gradlePlugin)
+    implementation(libs.kotlin.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("androidLibrary") {
+            id = "com.qure.library"
+            implementationClass = "gradle.plugin.AndroidLibraryPlugin"
+        }
+    }
+}
