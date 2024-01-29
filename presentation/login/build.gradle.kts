c4147978a596282d37a0288972efd48de444d0de
dltkd1395 <dltkd13956@naver.com>
2024-01-29 22:41:06 +0900

    chore: presentation login 모듈 build.gradle를 kts로 변경

---
+++ presentation/login/build.gradle.kts
@@ -0,0 +1,47 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    id("com.qure.library")
+    id("com.qure.hilt")
+    alias(libs.plugins.google.services)
+}
+
+val localPropertiesFile = rootProject.file("local.properties")
+val localProperties = Properties()
+localProperties.load(FileInputStream(localPropertiesFile))
+
+android {
+    namespace = "com.qure.login"
+    val removeQuotationKakaoApiKey =
+        properties["KAKAO_API_KEY"]?.let { id -> (id as String).replace("\"", "") } ?: ""
+    defaultConfig {
+        buildConfigField("String", "KAKAO_API_KEY", localProperties["kakao_api_key"] as String)
+        manifestPlaceholders["KAKAO_API_KEY"] = removeQuotationKakaoApiKey
+    }
+    buildFeatures {
+        dataBinding = true
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation(project(":build-property"))
+    implementation(project(":core-design"))
+    implementation(project(":core"))
+    implementation(project(":navigator"))
+    implementation(project(":domain"))
+    implementation(project(":data"))
+
+    api(libs.kakao.user)
+    api(libs.kakao.talk)
+    api(libs.kakao.story)
+    api(libs.kakao.share)
+    api(libs.jetpack.activity)
+    api(libs.network.core)
+    api(libs.network.adapter)
+    api(libs.network.interceptor)
+    api(platform(libs.firebase.bom))
+    api(libs.firebase.auth)
+    api(libs.firebase.analytics)
+}
