a871aedd4a69be8780d878d5e154b14bc344f194
dltkd1395 <dltkd13956@naver.com>
2024-01-29 22:40:40 +0900

    chore: presentation fishingspot 모듈 build.gradle를 kts로 변경

---
+++ presentation/fishingspot/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    id("com.qure.feature")
+}
+
+android {
+    namespace = "com.qure.fishingspot"
+}
+
+dependencies {
+    implementation(project(":core-design"))
+    implementation(project(":core"))
+    implementation(project(":navigator"))
+    implementation(project(":domain"))
+    implementation(project(":presentation:memo"))
+
+    api(libs.jetpack.activity)
+}
