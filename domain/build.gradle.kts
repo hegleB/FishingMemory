8e97b8f815ef98e6384af5678a31ee01fff4627b
dltkd1395 <dltkd13956@naver.com>
2024-01-29 22:37:57 +0900

    chore: domain 모듈 build.gradle를 kts로 변경

---
+++ domain/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.org.jetbrains.kotlin.jvm)
+}
+
+dependencies {
+    api(libs.inject)
+    api(libs.kotlin.coroutines)
+    api(libs.kotlin.stdilib)
+    api(libs.kotlin.coroutinesJvm)
+    api(libs.kotlin.coroutinesAndroid)
+}
