eb35f8b7c47f4bcb2284996eabce12d47bde002e
dltkd1395 <dltkd13956@naver.com>
2024-01-29 22:40:47 +0900

    chore: presentation gallary 모듈 build.gradle를 kts로 변경

---
+++ presentation/gallery/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.qure.feature")
+
+}
+
+android {
+    namespace = "com.qure.gallery"
+}
+
+dependencies {
+    implementation(project(":build-property"))
+    implementation(project(":core-design"))
+    implementation(project(":core"))
+    implementation(project(":navigator"))
+    implementation(project(":domain"))
+
+    api(libs.glide.core)
+    kapt(libs.hilt.compiler)
+}
