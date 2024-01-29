dcdd5ad42f967a3f3ab2352acfbb06375f3ebba7
dltkd1395 <dltkd13956@naver.com>
2024-01-29 22:41:22 +0900

    chore: presentation mypage 모듈 build.gradle를 kts로 변경

---
+++ presentation/mypage/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.qure.library")
+    id("com.qure.hilt")
+}
+
+android {
+    namespace = "com.qure.mypage"
+    buildFeatures {
+        dataBinding = true
+    }
+}
+
+dependencies {
+    implementation(project(":core-design"))
+    implementation(project(":core"))
+    implementation(project(":navigator"))
+    implementation(project(":domain"))
+
+    api(libs.jetpack.activity)
+    api(libs.jetpack.fragment)
+    api(libs.kakao.user)
+    kapt(libs.hilt.compiler)
+    api(libs.hilt.core)
+    api(libs.hilt.okhttp)
+}
