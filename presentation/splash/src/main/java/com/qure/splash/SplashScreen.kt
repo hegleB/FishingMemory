b28c821f7b8c862f8f58368b867d824365b77c4a
dltkd1395 <dltkd13956@naver.com>
2024-02-01 03:09:46 +0900

    feat: 스플래시 화면에서 다른 화면으로 전환

--- presentation/splash/src/main/java/com/qure/splash/SplashScreen.kt
+++ presentation/splash/src/main/java/com/qure/splash/SplashScreen.kt
@@ -1,3 +1,35 @@
+package com.qure.splash
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.qure.core_design.compose.components.FMLottieAnimation
+import com.qure.core_design.compose.utils.FMPreview
+import kotlinx.coroutines.delay
+
+@Composable
+fun SplashScreen(
+    viewModel: SplashViewModel,
+    navigateToOnBoarding: () -> Unit,
+    navigateToHome: () -> Unit,
+    navigateToLogin: () -> Unit,
+) {
+    val isFirstVisitor by viewModel.isFirstVisitor.collectAsStateWithLifecycle()
+    SplashContent(modifier = Modifier.fillMaxSize())
+    LaunchedEffect(Unit) {
+        delay(3500)
+        when {
+            isFirstVisitor -> navigateToOnBoarding()
+            viewModel.isSignedUp() -> navigateToHome()
+            else -> navigateToLogin()
+        }
+    }
+}
+
 @Composable
 private fun SplashContent(
     modifier: Modifier = Modifier,
@@ -6,4 +38,10 @@ private fun SplashContent(
         modifier = modifier,
         lottieId = R.raw.splash_logo,
     )
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() = FMPreview {
+    SplashContent()
 }
