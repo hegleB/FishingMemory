2019634dd6f1465a334bb53ab2a02b35e5160d13
dltkd1395 <dltkd13956@naver.com>
2024-02-01 02:56:19 +0900

    feat: BaseComposeFragment 추가

---
+++ core/src/main/java/com/qure/core/BaseComposeFragment.kt
@@ -0,0 +1,29 @@
+package com.qure.core
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.ViewCompositionStrategy
+import androidx.fragment.app.Fragment
+
+abstract class BaseComposeFragment : Fragment() {
+
+    @Composable
+    abstract fun Screen()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+            setContent {
+                Screen()
+            }
+        }
+    }
+}
