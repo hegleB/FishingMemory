d3209a58b3f3924f983812463e6fe5fa3bd4ee41
dltkd1395 <dltkd13956@naver.com>
2024-02-01 17:01:31 +0900

    feat: FMButton 추가

---
+++ core-design/src/main/java/com/qure/core_design/compose/components/FMButton.kt
@@ -0,0 +1,41 @@
+package com.qure.core_design.compose.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+
+@Composable
+fun FMButton(
+    modifier: Modifier = Modifier,
+    @StringRes textId: Int,
+    onClick: () -> Unit,
+    textStyle: TextStyle = TextStyle(),
+    textModifier: Modifier = Modifier,
+    buttonColor: Color = Color.White,
+    fontColor: Color = Color.Black,
+    shape: Shape = CircleShape,
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = buttonColor,
+        ),
+        shape = shape
+    ) {
+        Text(
+            modifier = textModifier,
+            text = stringResource(id = textId),
+            style = textStyle,
+            color = fontColor,
+        )
+    }
+}
