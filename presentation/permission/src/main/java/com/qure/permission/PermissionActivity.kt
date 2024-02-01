8fd0411f4814dcd543d345dc9b5fd60ff1bc818c
dltkd1395 <dltkd13956@naver.com>
2024-02-01 17:10:50 +0900

    feat: BaseComposeActivity를 통해 Compose Screen으로 수정

--- presentation/permission/src/main/java/com/qure/permission/PermissionActivity.kt
+++ presentation/permission/src/main/java/com/qure/permission/PermissionActivity.kt
@@ -1,82 +1,86 @@
 package com.qure.permission
 
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.net.Uri
-import android.os.Bundle
-import android.provider.Settings
-import androidx.core.app.ActivityCompat
-import com.qure.core.BaseActivity
-import com.qure.core.util.setOnSingleClickListener
+import androidx.compose.runtime.Composable
+import com.qure.core.BaseComposeActivity
+import com.qure.core_design.compose.theme.FishingMemoryTheme
 import com.qure.navigator.LoginNavigator
-import com.qure.permission.databinding.ActivityPermissionBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class PermissionActivity : BaseActivity<ActivityPermissionBinding>(R.layout.activity_permission) {
+class PermissionActivity : BaseComposeActivity() {
     @Inject
     lateinit var loginNavigator: LoginNavigator
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding.buttonActivityPermissionConfirm.setOnSingleClickListener {
-            if (checkPermission()) {
+    @Composable
+    override fun Screen() {
+        FishingMemoryTheme {
+            PermissionScreen(navigateToLogin = {
                 startActivity(loginNavigator.intent(this))
                 finish()
-            } else {
-                requestPermission()
-            }
+            })
         }
     }
-
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-            ),
-            PERMISSION_REQUEST_ACCESS_LOCATION,
-        )
-    }
-
-    private fun checkPermission(): Boolean {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
-            ) == PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            return true
-        }
-        return false
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray,
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        if (requestCode == PERMISSION_REQUEST_ACCESS_LOCATION) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                startActivity(loginNavigator.intent(this))
-                finish()
-            } else {
-                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                val uri: Uri = Uri.fromParts("package", packageName, null)
-                intent.data = uri
-                startActivity(intent)
-            }
-        }
-    }
-
-    companion object {
-        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
-    }
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        binding.buttonActivityPermissionConfirm.setOnSingleClickListener {
+//            if (checkPermission()) {
+//                startActivity(loginNavigator.intent(this))
+//                finish()
+//            } else {
+//                requestPermission()
+//            }
+//        }
+//    }
+//
+//    private fun requestPermission() {
+//        ActivityCompat.requestPermissions(
+//            this,
+//            arrayOf(
+//                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+//                android.Manifest.permission.ACCESS_FINE_LOCATION,
+//            ),
+//            PERMISSION_REQUEST_ACCESS_LOCATION,
+//        )
+//    }
+//
+//    private fun checkPermission(): Boolean {
+//        if (ActivityCompat.checkSelfPermission(
+//                this,
+//                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+//            ) == PackageManager.PERMISSION_GRANTED &&
+//            ActivityCompat.checkSelfPermission(
+//                this,
+//                android.Manifest.permission.ACCESS_FINE_LOCATION,
+//            ) == PackageManager.PERMISSION_GRANTED
+//        ) {
+//            return true
+//        }
+//        return false
+//    }
+//
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray,
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//
+//        if (requestCode == PERMISSION_REQUEST_ACCESS_LOCATION) {
+//            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                startActivity(loginNavigator.intent(this))
+//                finish()
+//            } else {
+//                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+//                val uri: Uri = Uri.fromParts("package", packageName, null)
+//                intent.data = uri
+//                startActivity(intent)
+//            }
+//        }
+//    }
+//
+//    companion object {
+//        private const val PERMISSION_REQUEST_ACCESS_LOCATION = 100
+//    }
 }
