87663bd7caae36284dae8797fad0c221478166ec
dltkd1395 <dltkd13956@naver.com>
2024-02-04 00:03:19 +0900

    refactor: 파라미터 이름 및 주석 추가

--- core-design/src/main/java/com/qure/core_design/compose/theme/Type.kt
+++ core-design/src/main/java/com/qure/core_design/compose/theme/Type.kt
@@ -21,32 +21,32 @@ val Typography = Typography(
     displayLarge = defaultTextStyle.copy(
         fontFamily = FontFamily(Font(R.font.sc_bold_dream)),
         fontSize = 17.sp,
-    ),
+    ), // Title1
     displayMedium = defaultTextStyle.copy(
         fontFamily = FontFamily(Font(R.font.sc_bold_dream)),
         fontSize = 15.sp,
-    ),
-    defaultTextStyle.copy(
+    ), // Title2
+    displaySmall = defaultTextStyle.copy(
         fontFamily = FontFamily(Font(R.font.sc_medium_dream)),
         fontSize = 12.sp,
-    ),
-    defaultTextStyle.copy(
+    ), // SubTitle1
+    headlineLarge = defaultTextStyle.copy(
         fontFamily = FontFamily(Font(R.font.sc_black_dream)),
         fontSize = 14.sp,
         lineHeight = 22.sp,
-    ),
-    defaultTextStyle.copy(
+    ), // Heading
+    bodyLarge = defaultTextStyle.copy(
         fontFamily = FontFamily(Font(R.font.sc_medium_dream)),
         fontSize = 15.sp,
         lineHeight = 24.sp,
-    ),
-    defaultTextStyle.copy(
+    ), // Paragraph1
+    bodyMedium = defaultTextStyle.copy(
         fontFamily = FontFamily(Font(R.font.sc_medium_dream)),
         fontSize = 14.sp,
-    ),
-    defaultTextStyle.copy(
+    ), // Paragraph2
+    bodySmall = defaultTextStyle.copy(
         fontFamily = FontFamily(Font(R.font.sc_medium_dream)),
         fontSize = 13.sp,
         lineHeight = 20.sp,
-    ),
+    ), // Paragraph3
 )
