43165e1e7733f99563baf3a85ba888dc0aac195f
dltkd1395 <dltkd13956@naver.com>
2024-02-05 09:23:26 +0900

    feat: BaseComposeActivity를 통해 Compose Screen으로 수정

--- presentation/login/src/main/java/com/qure/login/LoginActivity.kt
+++ presentation/login/src/main/java/com/qure/login/LoginActivity.kt
@@ -1,91 +1,27 @@
 package com.qure.login
 
-import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
-import com.kakao.sdk.common.model.ClientError
-import com.kakao.sdk.common.model.ClientErrorCause
-import com.kakao.sdk.user.UserApiClient
-import com.qure.core.BaseActivity
-import com.qure.core.util.FishingMemoryToast
-import com.qure.core.util.setOnSingleClickListener
-import com.qure.login.databinding.ActivityLoginBinding
-import com.qure.login.extension.loginWithKakaoOrThrow
+import androidx.compose.runtime.Composable
+import com.qure.core.BaseComposeActivity
+import com.qure.core_design.compose.theme.FishingMemoryTheme
 import com.qure.navigator.HomeNavigator
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class LoginActivity : BaseActivity<ActivityLoginBinding>(R.layout.activity_login) {
+class LoginActivity : BaseComposeActivity() {
     @Inject
     lateinit var homeNavigator: HomeNavigator
 
     protected val viewModel by viewModels<LoginViewModel>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        startKakaoLogin()
-        observe()
-    }
-
-    private fun startKakaoLogin() {
-        val context = this
-        binding.linearLayoutActivityLoginKakao.setOnSingleClickListener {
-            lifecycleScope.launch {
-                kotlin.runCatching {
-                    UserApiClient.loginWithKakaoOrThrow(context)
-                }.onSuccess { oAuthToken ->
-                    UserApiClient.instance.me { user, _ ->
-                        if (user != null) {
-                            user.kakaoAccount?.email?.let { email ->
-                                viewModel.createUser(
-                                    email = email,
-                                    accessToken = oAuthToken.accessToken,
-                                )
-                            }
-                        }
-                    }
-                }.onFailure { throwable ->
-                    if (throwable is ClientError && throwable.reason == ClientErrorCause.Cancelled) {
-                        FishingMemoryToast().error(
-                            this@LoginActivity,
-                            getString(R.string.message_kakao_cancellation_requested_User),
-                        )
-                    } else {
-                        FishingMemoryToast().error(
-                            this@LoginActivity,
-                            getString(R.string.message_kakao_login_failure),
-                        )
-                    }
-                }
-            }
+    @Composable
+    override fun Screen() {
+        FishingMemoryTheme {
+            LoginScreen(
+                viewModel = viewModel,
+                navigateToHome = { startActivity(homeNavigator.intent(this)); finish() },
+            )
         }
-    }
-
-    private fun observe() {
-        viewModel.error
-            .onEach { errorMessage ->
-                FishingMemoryToast().error(
-                    this,
-                    errorMessage,
-                )
-            }.launchIn(lifecycleScope)
-
-        viewModel.action.onEach { action ->
-            val intent =
-                when (action) {
-                    LoginViewModel.Action.AlreadySignUp -> {
-                        homeNavigator.intent(this)
-                    }
-                    LoginViewModel.Action.FirstSignUp -> {
-                        homeNavigator.intent(this)
-                    }
-                }
-            startActivity(intent)
-            finish()
-        }.launchIn(lifecycleScope)
     }
 }
