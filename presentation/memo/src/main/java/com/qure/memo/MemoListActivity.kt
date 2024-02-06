b5248500cd47b28d6b7030118933351c854fc5d6
dltkd1395 <dltkd13956@naver.com>
2024-02-06 18:25:55 +0900

    feat: BaseComposeActivity를 통해 Compose Screen으로 수정

--- presentation/memo/src/main/java/com/qure/memo/MemoListActivity.kt
+++ presentation/memo/src/main/java/com/qure/memo/MemoListActivity.kt
@@ -1,29 +1,17 @@
 package com.qure.memo
 
-import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.qure.core.BaseActivity
-import com.qure.core.extensions.gone
-import com.qure.core.extensions.initSwipeRefreshLayout
-import com.qure.core.extensions.visiable
-import com.qure.core.util.FishingMemoryToast
-import com.qure.core.util.setOnSingleClickListener
+import androidx.compose.runtime.Composable
+import com.qure.core.BaseComposeActivity
+import com.qure.core_design.compose.theme.FishingMemoryTheme
 import com.qure.domain.MEMO_DATA
-import com.qure.memo.databinding.ActivityMemoListBinding
 import com.qure.navigator.DetailMemoNavigator
 import com.qure.navigator.MemoCreateNavigator
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MemoListActivity : BaseActivity<ActivityMemoListBinding>(R.layout.activity_memo_list) {
+class MemoListActivity : BaseComposeActivity() {
     @Inject
     lateinit var detailMemoNavigator: DetailMemoNavigator
 
@@ -32,103 +20,19 @@ class MemoListActivity : BaseActivity<ActivityMemoListBinding>(R.layout.activity
 
     private val viewModel by viewModels<MemoListViewModel>()
 
-    private val adapter: MemoListAdapter by lazy {
-        MemoListAdapter(
-            onMemoClick = { memo ->
-                val intent = detailMemoNavigator.intent(this)
-                intent.putExtra(MEMO_DATA, memo)
-                startActivity(intent)
-            },
-        )
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        observe()
-        initView()
-        initRecyclerView()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        viewModel.getFilteredMemo()
-    }
-
-    private fun initView() {
-        binding.swipeRefreshLayoutActivityMemoList.initSwipeRefreshLayout()
-        binding.swipeRefreshLayoutActivityMemoList.setOnRefreshListener {
-            refreshMemoList()
+    @Composable
+    override fun Screen() {
+        FishingMemoryTheme {
+            MemoListScreen(
+                viewModel = viewModel,
+                onBack = { finish() },
+                navigateToMemoCreate = { startActivity(memoCreateNavigator.intent(this)) },
+                navigateToMemoDetail = { memo ->
+                    val intent = detailMemoNavigator.intent(this)
+                    intent.putExtra(MEMO_DATA, memo)
+                    startActivity(intent)
+                },
+            )
         }
-        binding.swipeRefreshLayoutActivityMemoList.setRefreshing(false)
-
-        binding.imageViewActivityMemoListBack.setOnSingleClickListener {
-            finish()
-        }
-
-        binding.lottieAnimationActivityMemoListFishing.apply {
-            setAnimation(R.raw.fishing)
-        }
-
-        binding.imageViewActivityMemoListAdd.setOnSingleClickListener {
-            startActivity(memoCreateNavigator.intent(this))
-        }
-    }
-
-    private fun refreshMemoList() =
-        lifecycleScope.launch {
-            viewModel.getFilteredMemo()
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-                    delay(500)
-                    viewModel.uiState.collect { uiState ->
-                        if (uiState.isFilterInitialized) {
-                            binding.swipeRefreshLayoutActivityMemoList.setRefreshing(false)
-                            adapter.submitList(uiState.filteredMemo) {
-                                binding.recyclerViewActivityMemoList.scrollToPosition(0)
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-    private fun observe() {
-        viewModel.error
-            .onEach { errorMessage -> FishingMemoryToast().error(this, errorMessage) }
-            .launchIn(lifecycleScope)
-
-        lifecycleScope.launch {
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-                    viewModel.uiState.collect {
-                        handleUiState(it)
-                    }
-                }
-            }
-        }
-    }
-
-    private fun handleUiState(uidState: UiState) {
-        if (uidState.isFilterInitialized) {
-            setViewVisiable(uidState)
-            binding.progressBarActivityMemoList.gone()
-        } else {
-            binding.progressBarActivityMemoList.visiable()
-        }
-    }
-
-    private fun setViewVisiable(uidState: UiState) {
-        if (uidState.filteredMemo.isEmpty()) {
-            binding.groupActivityMemoListEmpty.visiable()
-            binding.recyclerViewActivityMemoList.gone()
-        } else {
-            binding.groupActivityMemoListEmpty.gone()
-            binding.recyclerViewActivityMemoList.visiable()
-            adapter.submitList(uidState.filteredMemo)
-        }
-    }
-
-    private fun initRecyclerView() {
-        binding.recyclerViewActivityMemoList.adapter = adapter
     }
 }
