5f7931ed091f7372126580bf5e4bb4279e2a3bb8
dltkd1395 <dltkd13956@naver.com>
2024-02-06 17:58:04 +0900

    feat: BackButton 구현

---
+++ core-design/src/main/java/com/qure/core_design/compose/components/FMIconButton.kt
@@ -0,0 +1,24 @@
+package com.qure.core_design.compose.components
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+
+@Composable
+fun FMBackButton(
+    modifier: Modifier = Modifier,
+    onClickBack: () -> Unit,
+    backIcon: Painter,
+) {
+    IconButton(
+        modifier = modifier,
+        onClick = { onClickBack() },
+    ) {
+        Icon(
+            painter = backIcon,
+            contentDescription = null,
+        )
+    }
+}
