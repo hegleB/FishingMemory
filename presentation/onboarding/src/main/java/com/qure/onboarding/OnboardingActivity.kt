dd6d936baacfa19fd29aa302f10826f3047613fc
dltkd1395 <dltkd13956@naver.com>
2024-02-09 18:46:59 +0900

    feat: BaseComposeActivity를 통해 Compose Screen으로 수정

--- presentation/onboarding/src/main/java/com/qure/onboarding/OnboardingActivity.kt
+++ presentation/onboarding/src/main/java/com/qure/onboarding/OnboardingActivity.kt
@@ -1,131 +1,31 @@
 package com.qure.onboarding
 
-import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayoutMediator
-import com.qure.core.BaseActivity
-import com.qure.core.util.setOnSingleClickListener
+import androidx.compose.runtime.Composable
+import com.qure.core.BaseComposeActivity
+import com.qure.core_design.compose.theme.FishingMemoryTheme
 import com.qure.navigator.PermissionNavigator
-import com.qure.onboarding.databinding.ActivityOnboardingBinding
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class OnboardingActivity : BaseActivity<ActivityOnboardingBinding>(R.layout.activity_onboarding) {
+class OnboardingActivity : BaseComposeActivity() {
     @Inject
     lateinit var permissionNavigator: PermissionNavigator
 
     private val viewModel by viewModels<OnboardingViewModel>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        initViewPager()
-        initEvent()
-    }
-
-    private fun initViewPager() {
-        binding.apply {
-            viewpagerActivityOnboardingOnboarding.adapter =
-                ScreenSlidePagerAdapter(this@OnboardingActivity)
-            viewpagerActivityOnboardingOnboarding.orientation = ViewPager2.ORIENTATION_HORIZONTAL
-            viewpagerActivityOnboardingOnboarding.isUserInputEnabled = false
-        }
-
-        TabLayoutMediator(
-            binding.tabLayoutActivityOnboardingDot,
-            binding.viewpagerActivityOnboardingOnboarding,
-        ) { tab, position -> }.attach()
-    }
-
-    private fun initEvent() {
-        setViewPagePosition()
-        setButtonTextToPageTransition()
-    }
-
-    private fun setViewPagePosition() {
-        binding.apply {
-            buttonActivityOnboardingNext.setOnSingleClickListener {
-                viewpagerActivityOnboardingOnboarding.run {
-                    if (currentItem == END_PAGE) {
-                        viewModel.writeFirstVisitor()
-                        startActivity(permissionNavigator.intent(this@OnboardingActivity))
-                        finish()
-                    } else {
-                        currentItem += PAGE_INCREMENT_VALUE
-                    }
-                }
-            }
-        }
-    }
-
-    private fun setButtonTextToPageTransition() {
-        binding.apply {
-            viewpagerActivityOnboardingOnboarding.registerOnPageChangeCallback(
-                object :
-                    ViewPager2.OnPageChangeCallback() {
-                    override fun onPageSelected(position: Int) {
-                        super.onPageSelected(position)
-                        when (position) {
-                            OnboardingPage.RECORD_PAGE.position -> buttonActivityOnboardingNext.text = getString(R.string.onboarding_next)
-                            OnboardingPage.STATISTICS_PAGE.position ->
-                                buttonActivityOnboardingNext.text =
-                                    getString(
-                                        R.string.onboarding_next,
-                                    )
-                            else -> {
-                                buttonActivityOnboardingNext.text =
-                                    getString(R.string.onboarding_start)
-                            }
-                        }
-                    }
+    @Composable
+    override fun Screen() {
+        FishingMemoryTheme {
+            OnboardingScreen(
+                viewModel = viewModel,
+                navigateToPermission = {
+                    viewModel.writeFirstVisitor()
+                    startActivity(permissionNavigator.intent(this@OnboardingActivity))
+                    finish()
                 },
             )
         }
     }
-
-    private inner class ScreenSlidePagerAdapter(fragmentActivity: FragmentActivity) :
-        FragmentStateAdapter(fragmentActivity) {
-        override fun getItemCount(): Int = PAGES_NUMBER
-
-        override fun createFragment(position: Int): Fragment {
-            return when (position) {
-                OnboardingPage.RECORD_PAGE.position -> {
-                    OnboardingFragment.newInstance(
-                        title = getString(R.string.onboarding_first_title),
-                        onboardingLottieResId = R.raw.first_page,
-                    )
-                }
-
-                OnboardingPage.STATISTICS_PAGE.position -> {
-                    OnboardingFragment.newInstance(
-                        title = getString(R.string.onboarding_second_title),
-                        onboardingLottieResId = R.raw.second_page,
-                    )
-                }
-
-                else -> {
-                    OnboardingFragment.newInstance(
-                        title = getString(R.string.onboarding_third_title),
-                        onboardingLottieResId = R.raw.third_page,
-                    )
-                }
-            }
-        }
-    }
-
-    companion object {
-        private const val PAGES_NUMBER = 3
-        private const val PAGE_INCREMENT_VALUE = 1
-        private const val END_PAGE = 2
-    }
-}
-
-enum class OnboardingPage(val position: Int) {
-    RECORD_PAGE(0),
-    STATISTICS_PAGE(1),
 }
