0f691eb85cfc050e60f734829bd20186233025a3
dltkd1395 <dltkd13956@naver.com>
2024-02-11 23:31:41 +0900

    feat: 북마크 가져오도록 초기화

--- presentation/fishingspot/src/main/java/com/qure/fishingspot/bookmark/BookmarkViewModel.kt
+++ presentation/fishingspot/src/main/java/com/qure/fishingspot/bookmark/BookmarkViewModel.kt
@@ -16,44 +16,51 @@ import javax.inject.Inject
 
 @HiltViewModel
 class BookmarkViewModel
-    @Inject
-    constructor(
-        private val getFishingSpotBookmarksUseCase: GetFishingSpotBookmarksUseCase,
-        private val deleteAllFishingSpotBookmarkUseCase: DeleteAllFishingSpotBookmarkUseCase,
-    ) : BaseViewModel() {
-        private val _fishingSpotBookmarks: MutableStateFlow<List<FishingSpotUI>> =
-            MutableStateFlow(emptyList())
-        val fishingSpotBookmarks: StateFlow<List<FishingSpotUI>>
-            get() = _fishingSpotBookmarks
+@Inject
+constructor(
+    private val getFishingSpotBookmarksUseCase: GetFishingSpotBookmarksUseCase,
+    private val deleteAllFishingSpotBookmarkUseCase: DeleteAllFishingSpotBookmarkUseCase,
+) : BaseViewModel() {
+    private val _fishingSpotBookmarks: MutableStateFlow<List<FishingSpotUI>> =
+        MutableStateFlow(emptyList())
+    val fishingSpotBookmarks: StateFlow<List<FishingSpotUI>>
+        get() = _fishingSpotBookmarks
 
-        fun getBookmarks() {
-            viewModelScope.launch(Dispatchers.IO) {
-                getFishingSpotBookmarksUseCase().collect { response ->
-                    when (response) {
-                        is Result.Loading -> {
-                            startLoading()
-                        }
-                        is Result.Error -> {
-                            stopLoading()
-                            sendErrorMessage(response.exception)
-                        }
-                        is Result.Empty -> {
-                            stopLoading()
-                            _fishingSpotBookmarks.value = emptyList()
-                        }
-                        is Result.Success -> {
-                            _fishingSpotBookmarks.value = response.data.map { it.toFishingSpotUI() }
-                            stopLoading()
-                        }
+    init {
+        getBookmarks()
+    }
+
+    fun getBookmarks() {
+        viewModelScope.launch(Dispatchers.IO) {
+            getFishingSpotBookmarksUseCase().collect { response ->
+                when (response) {
+                    is Result.Loading -> {
+                        startLoading()
+                    }
+
+                    is Result.Error -> {
+                        stopLoading()
+                        sendErrorMessage(response.exception)
+                    }
+
+                    is Result.Empty -> {
+                        stopLoading()
+                        _fishingSpotBookmarks.value = emptyList()
+                    }
+
+                    is Result.Success -> {
+                        _fishingSpotBookmarks.value = response.data.map { it.toFishingSpotUI() }
+                        stopLoading()
                     }
                 }
             }
         }
+    }
 
-        fun deleteAllBookmarks() {
-            viewModelScope.launch {
-                deleteAllFishingSpotBookmarkUseCase()
-                _fishingSpotBookmarks.value = emptyList()
-            }
+    fun deleteAllBookmarks() {
+        viewModelScope.launch {
+            deleteAllFishingSpotBookmarkUseCase()
+            _fishingSpotBookmarks.value = emptyList()
         }
     }
+}
