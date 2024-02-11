3a18671fb313e592a7e865e55a5b98fa589148b2
dltkd1395 <dltkd13956@naver.com>
2024-02-11 23:32:07 +0900

    feat: BaseComposeActivity를 통해 Compose Screen으로 수정

--- presentation/fishingspot/src/main/java/com/qure/fishingspot/bookmark/BookmarkActivity.kt
+++ presentation/fishingspot/src/main/java/com/qure/fishingspot/bookmark/BookmarkActivity.kt
@@ -2,112 +2,38 @@ package com.qure.fishingspot.bookmark
 
 import android.content.Intent
 import android.net.Uri
-import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
-import com.qure.core.BaseActivity
-import com.qure.core.extensions.gone
-import com.qure.core.extensions.visiable
-import com.qure.core.util.FishingMemoryToast
-import com.qure.core.util.setOnSingleClickListener
+import androidx.compose.runtime.Composable
+import com.qure.core.BaseComposeActivity
+import com.qure.core_design.compose.theme.FishingMemoryTheme
 import com.qure.domain.SPOT_DATA
-import com.qure.fishingspot.R
-import com.qure.fishingspot.databinding.ActivityBookmarkBinding
 import com.qure.navigator.FishingSpotNavigator
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class BookmarkActivity : BaseActivity<ActivityBookmarkBinding>(R.layout.activity_bookmark), OnDeleteClickListener {
+class BookmarkActivity : BaseComposeActivity() {
     @Inject
     lateinit var fishingSpotNavigator: FishingSpotNavigator
 
     private val viewModel by viewModels<BookmarkViewModel>()
-    private val adapter: BookmarkAdatper by lazy {
-        BookmarkAdatper({
-            val intent = fishingSpotNavigator.intent(this)
-            intent.putExtra(SPOT_DATA, it)
-            startActivity(intent)
-        }, {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:$it"))
-            startActivity(intent)
-        })
-    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        initRecyclerView()
-        initView()
-        observe()
-    }
-
-    private fun initView() {
-        binding.imageViewActivityBookmarkBack.setOnSingleClickListener {
-            finish()
-        }
-
-        binding.textViewActivityBookmarkDeleteAll.setOnSingleClickListener {
-            onDeleteAllClicked()
-        }
-    }
-
-    private fun onDeleteAllClicked() {
-        BookmarkDeleteDialogFragment()
-            .show(
-                this.supportFragmentManager,
-                BookmarkDeleteDialogFragment.TAG,
+    @Composable
+    override fun Screen() {
+        FishingMemoryTheme {
+            BookmarkScreen(
+                viewModel = viewModel,
+                onBack = { finish() },
+                navigateToFishingSpot = { fishingSpot ->
+                    val intent = fishingSpotNavigator.intent(this)
+                    intent.putExtra(SPOT_DATA, fishingSpot)
+                    startActivity(intent)
+                },
+                onClickPhoneNumber = { phoneNumber ->
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse("tel:$phoneNumber"))
+                    startActivity(intent)
+                },
             )
-    }
-
-    private fun initRecyclerView() {
-        binding.recyclerViewActivityBookmark.adapter = adapter
-    }
-
-    private fun observe() {
-        viewModel.error
-            .onEach { errorMessage -> FishingMemoryToast().error(this, errorMessage) }
-            .launchIn(lifecycleScope)
-
-        lifecycleScope.launch {
-            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                launch {
-                    viewModel.fishingSpotBookmarks.collect {
-                        if (it.isEmpty()) {
-                            binding.textViewActivityBookmarkEmpty.visiable()
-                            binding.recyclerViewActivityBookmark.gone()
-                        } else {
-                            binding.textViewActivityBookmarkEmpty.gone()
-                            binding.recyclerViewActivityBookmark.visiable()
-                            adapter.submitList(it)
-                        }
-                    }
-                }
-
-                launch {
-                    viewModel.isLoading.collect {
-                        if (it) {
-                            binding.progressBarActivityBookmark.visiable()
-                        } else {
-                            binding.progressBarActivityBookmark.gone()
-                        }
-                    }
-                }
-            }
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        viewModel.getBookmarks()
-    }
-
-    override fun onDeleteClicked() {
-        viewModel.deleteAllBookmarks()
     }
 }
