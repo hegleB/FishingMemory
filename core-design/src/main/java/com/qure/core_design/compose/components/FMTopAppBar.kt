c7adbbb423691f56a4580ec0db924bf577aadf5b
dltkd1395 <dltkd13956@naver.com>
2024-02-11 17:00:49 +0900

    refactor: 파라미터 기본 값 재설정

--- core-design/src/main/java/com/qure/core_design/compose/components/FMTopAppBar.kt
+++ core-design/src/main/java/com/qure/core_design/compose/components/FMTopAppBar.kt
@@ -12,23 +12,26 @@ import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
 import com.qure.core_design.compose.utils.FMPreview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FMTopAppBar(
-    title: String,
-    titleFontSize: TextUnit = TextUnit.Unspecified,
-    titleColor: Color = Color.Unspecified,
-    navigationIconColor: Color = Color.Unspecified,
-    containerColor: Color = Color.Unspecified,
-    onBack: () -> Unit,
+    title: String = "",
+    titleFontSize: TextUnit = 22.sp,
+    titleColor: Color = MaterialTheme.colorScheme.onBackground,
+    navigationIconColor: Color = MaterialTheme.colorScheme.onBackground,
+    containerColor: Color = MaterialTheme.colorScheme.background,
+    onBack: () -> Unit = { },
     onClick: () -> Unit = { },
     hasActionIcon: Boolean = false,
     @DrawableRes actionIconRes: Int? = null,
     actions: @Composable (RowScope.() -> Unit) = { },
+    style: TextStyle = MaterialTheme.typography.displayLarge,
 ) {
     CenterAlignedTopAppBar(
         modifier = Modifier
@@ -36,7 +39,7 @@ fun FMTopAppBar(
         title = {
             Text(
                 text = title,
-                style = MaterialTheme.typography.displayLarge,
+                style = style,
                 fontSize = titleFontSize,
                 color = titleColor,
             )
@@ -58,9 +61,5 @@ fun FMTopAppBar(
 @Preview
 @Composable
 private fun FMTopAppBarPreview() = FMPreview {
-    FMTopAppBar(
-        title = "TopAppBar",
-        onBack = { },
-        onClick = { },
-    )
+    FMTopAppBar()
 }
