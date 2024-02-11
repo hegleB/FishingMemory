3b6a9e12c971d86193957ab27722a9615485b1f1
dltkd1395 <dltkd13956@naver.com>
2024-02-11 16:33:51 +0900

    feat: BookmarkButton 추가

--- core-design/src/main/java/com/qure/core_design/compose/components/FMIconButton.kt
+++ core-design/src/main/java/com/qure/core_design/compose/components/FMIconButton.kt
@@ -1,13 +1,24 @@
 package com.qure.core_design.compose.components
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
 import com.qure.core_design.R
+import com.qure.core_design.compose.utils.clickableWithoutRipple
 
 @Composable
 fun FMBackButton(
@@ -45,22 +56,35 @@ fun FMCircleAddButton(
     }
 }
 
-//@Composable
-//fun FMFavoriteButton(
-//    modifier: Modifier = Modifier,
-//    onClickFavorite: () -> Unit,
-//    favoriteIcon: Painter,
-//    iconColor: Color = Color.Unspecified,
-//) {
-//    val favoriteIcon = if (isFavorite) R.raw.start else R.raw.
-//    IconButton(
-//        modifier = modifier,
-//        onClick = { onClickFavorite },
-//    ) {
-//        Icon(
-//            painter = if (isFavorite) ,
-//            contentDescription = null,
-//            tint = iconColor,
-//        )
-//    }
-//}
+@Composable
+fun FMBookmarkButton(
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit,
+    isBookmarkClicked: Boolean = false,
+) {
+    val composition by rememberLottieComposition(
+        LottieCompositionSpec.RawRes(R.raw.star),
+    )
+
+    val initialProgress = if (isBookmarkClicked) 0.4f else 0f
+    val animatableProgress = remember { Animatable(initialProgress) }
+
+    LaunchedEffect(isBookmarkClicked) {
+        val targetValue = if (isBookmarkClicked) 0.6f else 0f
+        animatableProgress.animateTo(
+            targetValue = targetValue,
+            animationSpec = tween(durationMillis = if (isBookmarkClicked) 1000 else 700),
+        )
+    }
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .clickableWithoutRipple { onClick() },
+    ) {
+        LottieAnimation(
+            composition = composition,
+            contentScale = ContentScale.FillHeight,
+            progress = { animatableProgress.value },
+        )
+    }
+}
