3674f55108e967fc3df6cc1730da1d6c55db1be9
dltkd1395 <dltkd13956@naver.com>
2024-02-12 23:09:37 +0900

    feat: BaseComposeActivity를 통해 Compose Screen으로 수정

--- presentation/memo/src/main/java/com/qure/memo/detail/DetailMemoActivity.kt
+++ presentation/memo/src/main/java/com/qure/memo/detail/DetailMemoActivity.kt
@@ -2,53 +2,41 @@ package com.qure.memo.detail
 
 import android.content.Intent
 import android.net.Uri
-import android.os.Bundle
-import android.view.View
-import android.widget.PopupMenu
 import androidx.activity.viewModels
-import androidx.lifecycle.lifecycleScope
-import com.bumptech.glide.Glide
-import com.qure.core.BaseActivity
+import androidx.compose.runtime.Composable
+import com.qure.core.BaseComposeActivity
 import com.qure.core.extensions.Empty
-import com.qure.core.extensions.gone
-import com.qure.core.util.FishingMemoryToast
-import com.qure.core.util.setOnSingleClickListener
+import com.qure.core_design.compose.theme.FishingMemoryTheme
 import com.qure.domain.MEMO_DATA
 import com.qure.domain.UPDATE_MEMO
-import com.qure.memo.R
-import com.qure.memo.databinding.ActivityDetailMemoBinding
-import com.qure.memo.delete.DeleteDialogFragment
 import com.qure.memo.model.MemoUI
-import com.qure.memo.share.ShareDialogFragment
 import com.qure.navigator.MemoCreateNavigator
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class DetailMemoActivity : BaseActivity<ActivityDetailMemoBinding>(R.layout.activity_detail_memo) {
+class DetailMemoActivity : BaseComposeActivity() {
     @Inject
     lateinit var memoCreateNavigator: MemoCreateNavigator
 
     private val viewModel by viewModels<DetailMemoViewModel>()
     private var memo: MemoUI = MemoUI()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        observe()
-    }
-
-    override fun onStart() {
-        super.onStart()
+    @Composable
+    override fun Screen() {
         initData()
-        initView()
-    }
-
-    private fun observe() {
-        viewModel.error
-            .onEach { errorMessage -> FishingMemoryToast().error(this, errorMessage) }
-            .launchIn(lifecycleScope)
+        FishingMemoryTheme {
+            DetailMemoScreen(
+                memo = memo,
+                viewModel = viewModel,
+                onBack = { finish() },
+                onClickEdit = {
+                    val intent = memoCreateNavigator.intent(this)
+                    intent.putExtra(UPDATE_MEMO, memo)
+                    startActivity(intent)
+                },
+            )
+        }
     }
 
     private fun initData() {
@@ -56,15 +44,18 @@ class DetailMemoActivity : BaseActivity<ActivityDetailMemoBinding>(R.layout.acti
             when {
                 intent.getParcelableExtra<MemoUI>(UPDATE_MEMO) != null ->
                     intent.getParcelableExtra(UPDATE_MEMO) ?: MemoUI()
+
                 Intent.ACTION_VIEW == intent.action -> {
                     val uri = intent.data
-                    binding.imageViewActivityDetailMemoMore.gone()
+//                    binding.imageViewActivityDetailMemoMore.gone()
                     uri?.let { createMemoUI(it) } ?: MemoUI()
                 }
+
                 else -> intent.getParcelableExtra(MEMO_DATA) ?: MemoUI()
             }
     }
 
+    //
     fun createMemoUI(uri: Uri): MemoUI {
         return MemoUI(
             title = uri.getQueryParameter(QUERY_TITLE) ?: String.Empty,
@@ -75,73 +66,9 @@ class DetailMemoActivity : BaseActivity<ActivityDetailMemoBinding>(R.layout.acti
             location = uri.getQueryParameter(QUERY_LOCATION) ?: String.Empty,
             content = uri.getQueryParameter(QUERY_CONTENT) ?: String.Empty,
             image =
-                uri.getQueryParameter(QUERY_BASE_URL) + SLASH +
-                    uri.getQueryParameter(QUERY_IMAGE_PATH),
+            uri.getQueryParameter(QUERY_BASE_URL) + SLASH +
+                uri.getQueryParameter(QUERY_IMAGE_PATH),
         )
-    }
-
-    private fun initView() {
-        setMemoView()
-        binding.apply {
-            imageViewActivityDetailMemoBack.setOnSingleClickListener {
-                finish()
-            }
-
-            imageViewActivityDetailMemoMore.setOnSingleClickListener {
-                showPopUp(it)
-            }
-        }
-    }
-
-    private fun showPopUp(view: View) {
-        val popup = PopupMenu(this, view)
-        popup.inflate(R.menu.menu_detail_memo)
-        popup.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.menu_share -> {
-                    ShareDialogFragment.newInstance(memo)
-                        .show(
-                            this.supportFragmentManager,
-                            ShareDialogFragment.TAG,
-                        )
-                    true
-                }
-                R.id.menu_delete -> {
-                    DeleteDialogFragment.newInstance(memo.uuid)
-                        .show(
-                            this.supportFragmentManager,
-                            DeleteDialogFragment.TAG,
-                        )
-                    true
-                }
-                R.id.menu_edit -> {
-                    val intent = memoCreateNavigator.intent(this)
-                    intent.putExtra(UPDATE_MEMO, memo)
-                    startActivity(intent)
-                    finish()
-                    true
-                }
-                else -> false
-            }
-        }
-        popup.show()
-    }
-
-    private fun setMemoView() {
-        binding.apply {
-            textViewActivityDetailMemoFishNameHeadline.text = memo.fishType
-            textViewActivityDetailMemoFishName.text = memo.fishType
-            textViewActivityDetailMemoWaterType.text = memo.waterType
-            textViewActivityDetailMemoFishSize.text = memo.fishSize + SIZE_UNIT
-            textViewActivityDetailMemoContent.text = memo.content
-            textViewActivityDetailMemoLocation.text = memo.location
-            textViewActivityDetailMemoCreateTime.text = memo.date
-            textViewActivityDetailMemoTitle.text = memo.title
-
-            Glide.with(this@DetailMemoActivity)
-                .load(memo.image)
-                .into(imageViewActivityDetailMemoFishImage)
-        }
     }
 
     companion object {
