c390ba1ded4219cb11ec3adccab90701ada49e76
dltkd1395 <dltkd13956@naver.com>
2024-05-12 20:49:05 +0900

    refactor: BaseComposeFragment로 변경

--- presentation/history/src/main/java/com/qure/history/HistoryFragment.kt
+++ presentation/history/src/main/java/com/qure/history/HistoryFragment.kt
@@ -1,8 +1,10 @@
 package com.qure.history
 
+import androidx.compose.runtime.Composable
 import androidx.fragment.app.activityViewModels
+import com.qure.core.BaseComposeFragment
+import com.qure.core_design.compose.theme.FishingMemoryTheme
 import com.qure.domain.MEMO_DATA
-import com.qure.history.databinding.FragmentHistoryBinding
 import com.qure.navigator.DetailMemoNavigator
 import com.qure.navigator.MapNavigator
 import com.qure.navigator.MemoCreateNavigator
@@ -10,7 +12,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class HistoryFragment : BaseFragment<FragmentHistoryBinding>(R.layout.fragment_history) {
+class HistoryFragment : BaseComposeFragment() {
     @Inject
     lateinit var memoCreateNavigator: MemoCreateNavigator
 
@@ -21,7 +23,22 @@ class HistoryFragment : BaseFragment<FragmentHistoryBinding>(R.layout.fragment_h
     lateinit var mapNavigator: MapNavigator
 
     private val viewModel by activityViewModels<HistoryViewModel>()
+    
+    @Composable
+    override fun Screen() {
+        FishingMemoryTheme {
+            HistoryScreen(
+                viewModel = viewModel,
+                navigateToMap = { startActivity(mapNavigator.intent(requireContext())) },
+                navigateToMemoCreate = { startActivity(memoCreateNavigator.intent(requireContext())) },
+                navigateToMemoDetail = { memo ->
+                    val intent = detailMemoNavigator.intent(requireContext())
+                    intent.putExtra(MEMO_DATA, memo)
+                    startActivity(intent)
                 },
+                onSelectedMonthChange = viewModel::selectMonth,
+                onSelectedDayChange = viewModel::selectDate,
+                onSelectedYearChange = viewModel::selectYear,
             )
         }
     }
