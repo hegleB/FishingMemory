4fe0830307be7034161433425ecf1db9131c05eb
dltkd1395 <dltkd13956@naver.com>
2024-05-14 21:07:43 +0900

    feat: NaverMap 추가

---
+++ core-design/src/main/java/com/qure/core_design/compose/components/FMNaverMap.kt
@@ -0,0 +1,37 @@
+package com.qure.core_design.compose.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.naver.maps.map.compose.CameraPositionState
+import com.naver.maps.map.compose.ExperimentalNaverMapApi
+import com.naver.maps.map.compose.Marker
+import com.naver.maps.map.compose.MarkerState
+import com.naver.maps.map.compose.NaverMap
+import com.naver.maps.map.compose.rememberCameraPositionState
+import com.naver.maps.map.overlay.OverlayImage
+import com.qure.core_design.R
+
+@OptIn(ExperimentalNaverMapApi::class)
+@Composable
+fun FMNaverMap(
+    modifier: Modifier = Modifier,
+    cameraPositionState: CameraPositionState = rememberCameraPositionState(),
+    markerState: MarkerState = MarkerState(),
+    markerHeight: Dp = 0.dp,
+    markerWidth: Dp = 0.dp,
+    icon: OverlayImage = OverlayImage.fromResource(R.drawable.bg_map_fill_marker),
+) {
+    NaverMap(
+        modifier = modifier,
+        cameraPositionState = cameraPositionState,
+    ) {
+        Marker(
+            state = markerState,
+            height = markerHeight,
+            width = markerWidth,
+            icon = icon,
+        )
+    }
+}
