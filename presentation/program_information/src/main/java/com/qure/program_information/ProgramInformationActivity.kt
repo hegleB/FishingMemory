7e25e18285b875e0719d0f51b812edda00c6c2c4
dltkd1395 <dltkd13956@naver.com>
2024-05-14 20:48:45 +0900

    refactor: BaseComposeActivity 변경

--- presentation/program_information/src/main/java/com/qure/program_information/ProgramInformationActivity.kt
+++ presentation/program_information/src/main/java/com/qure/program_information/ProgramInformationActivity.kt
@@ -1,34 +1,22 @@
 package com.qure.program_information
 
-import android.os.Bundle
-import com.qure.core.BaseActivity
+import androidx.compose.runtime.Composable
+import com.qure.core.BaseComposeActivity
 import com.qure.core.extensions.Empty
-import com.qure.core.util.setOnSingleClickListener
+import com.qure.core_design.compose.theme.FishingMemoryTheme
 import com.qure.domain.WEB_URL
-import com.qure.program_information.databinding.ActivityProgramInformationBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class ProgramInformationActivity :
-    BaseActivity<ActivityProgramInformationBinding>(R.layout.activity_program_information) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        initView()
-    }
-
-    private fun initView() {
+class ProgramInformationActivity : BaseComposeActivity() {
+    @Composable
+    override fun Screen() {
         val webUrl = intent.getStringExtra(WEB_URL) ?: String.Empty
-
-        binding.imageViewActivityPolicyClose.setOnSingleClickListener {
-            finish()
+        FishingMemoryTheme {
+            ProgramInformationScreen(
+                webUrl = webUrl,
+                onClose = { finish() },
+            )
         }
-
-        binding.webViewActivityPolicy.settings.apply {
-            javaScriptEnabled = true
-            loadWithOverviewMode = true
-            useWideViewPort = true
-        }
-
-        binding.webViewActivityPolicy.loadUrl(webUrl)
     }
 }
