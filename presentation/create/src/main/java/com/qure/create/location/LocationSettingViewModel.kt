35e7ca2c7a126a260b8469963df78caff4756bad
dltkd1395 <dltkd13956@naver.com>
2024-05-15 23:40:47 +0900

    feat: doIndex, cityIndex, regions, selectedRegions 설정 함수 추가

--- presentation/create/src/main/java/com/qure/create/location/LocationSettingViewModel.kt
+++ presentation/create/src/main/java/com/qure/create/location/LocationSettingViewModel.kt
@@ -10,76 +10,122 @@ import com.qure.create.model.toReverseGeocodingUI
 import com.qure.domain.usecase.map.GetGeocodingUseCase
 import com.qure.domain.usecase.map.GetReverseGeocodingUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class LocationSettingViewModel
-    @Inject
-    constructor(
-        private val getGeocodingUseCase: GetGeocodingUseCase,
-        private val getReverseGeocodingUseCase: GetReverseGeocodingUseCase,
-    ) : BaseViewModel() {
-        private val _uiState = MutableStateFlow(UiState())
-        val uiState: StateFlow<UiState>
-            get() = _uiState
+@Inject
+constructor(
+    private val getGeocodingUseCase: GetGeocodingUseCase,
+    private val getReverseGeocodingUseCase: GetReverseGeocodingUseCase,
+) : BaseViewModel() {
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState: StateFlow<UiState>
+        get() = _uiState
 
-        private val _uiEffect: MutableSharedFlow<UiEffect> = MutableSharedFlow()
-        val uiEffect: SharedFlow<UiEffect>
-            get() = _uiEffect
+    private val _uiEffect: MutableSharedFlow<UiEffect> = MutableSharedFlow()
+    val uiEffect: SharedFlow<UiEffect>
+        get() = _uiEffect
 
-        fun getGeocoding(query: String) {
-            viewModelScope.launch {
-                getGeocodingUseCase(query).collect { response ->
-                    response.onSuccess { geocoding ->
-                        if (geocoding.status == "OK") {
-                            _uiState.update {
-                                it.copy(
-                                    geocodingUI = geocoding.toGeocodingUI(),
-                                    isGeocodingInitialized = true,
-                                )
-                            }
+    fun getGeocoding(query: String) {
+        viewModelScope.launch {
+            getGeocodingUseCase(query).collect { response ->
+                response.onSuccess { geocoding ->
+                    if (geocoding.status == "OK") {
+                        _uiState.update {
+                            it.copy(
+                                geocodingUI = geocoding.toGeocodingUI(),
+                                isGeocodingInitialized = true,
+                            )
                         }
-                    }.onFailure { throwable ->
-                        throwable as Exception
                     }
+                }.onFailure { throwable ->
+                    throwable as Exception
                 }
-            }
-        }
-
-        fun getReverseGeocoding(coords: String) {
-            viewModelScope.launch {
-                getReverseGeocodingUseCase(coords).collect { response ->
-                    response.onSuccess { reverseGeocoding ->
-                        if (reverseGeocoding.status.code == 0) {
-                            _uiState.update {
-                                it.copy(
-                                    reverseGeocodingUI = reverseGeocoding.toReverseGeocodingUI(),
-                                    isReverseGeocodingInittialized = true,
-                                    geocodingUI = GeocodingUI(coords = coords),
-                                )
-                            }
-                        } else {
-                            onFailMarkerClick()
-                        }
-                    }.onFailure { throwable ->
-                        throwable as Exception
-                    }
-                }
-            }
-        }
-
-        fun onFailMarkerClick() {
-            viewModelScope.launch {
-                _uiEffect.emit(
-                    UiEffect.ShowToastMessage(
-                        resId = R.string.location_marker_failure_message,
-                    ),
-                )
             }
         }
     }
+
+    fun getReverseGeocoding(coords: String) {
+        viewModelScope.launch {
+            getReverseGeocodingUseCase(coords).collect { response ->
+                response.onSuccess { reverseGeocoding ->
+                    if (reverseGeocoding.status.code == 0) {
+                        _uiState.update {
+                            it.copy(
+                                reverseGeocodingUI = reverseGeocoding.toReverseGeocodingUI(),
+                                isReverseGeocodingInitialized = true,
+                                geocodingUI = GeocodingUI(coords = coords),
+                            )
+                        }
+                    } else {
+                        onFailMarkerClick()
+                    }
+                }.onFailure { throwable ->
+                    throwable as Exception
+                }
+            }
+        }
+    }
+
+    fun onFailMarkerClick() {
+        viewModelScope.launch {
+            _uiEffect.emit(
+                UiEffect.ShowToastMessage(
+                    resId = R.string.location_marker_failure_message,
+                ),
+            )
+        }
+    }
+
+    fun onClickNext() {
+        val currentPage = _uiState.value.currentPage
+        if (currentPage < 2) {
+            _uiState.update {
+                it.copy(currentPage = currentPage.plus(1))
+            }
+        }
+    }
+
+    fun onClickPrevious() {
+        val currentPage = _uiState.value.currentPage
+        if (currentPage > 0) {
+            _uiState.update {
+                it.copy(currentPage = currentPage.minus(1))
+            }
+        }
+    }
+
+    fun setDoIndex(index: Int) {
+        _uiState.update {
+            it.copy(doIndex = index)
+        }
+    }
+
+    fun setCityIndex(index: Int) {
+        _uiState.update {
+            it.copy(cityIndex = index)
+        }
+    }
+
+    fun setRegions(regions: List<String>) {
+        _uiState.update {
+            it.copy(regions = regions)
+        }
+    }
+
+    fun setSelectedRegions(regions: List<String>) {
+        _uiState.update {
+            it.copy(selectedRegions = regions)
+        }
+    }
+}
 
 data class UiState(
     val geocodingUI: GeocodingUI? = null,
