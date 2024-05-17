92ce119010f70715aeca81dd92258a6c27901edc
dltkd1395 <dltkd13956@naver.com>
2024-05-17 23:14:12 +0900

    refactor: BaseComposeActivity 변경 및 불필요한 코드 제거

--- presentation/gallery/src/main/java/com/qure/gallery/GalleryActivity.kt
+++ presentation/gallery/src/main/java/com/qure/gallery/GalleryActivity.kt
@@ -1,134 +1,50 @@
 package com.qure.gallery
 
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.net.Uri
-import android.os.Bundle
 import android.os.Environment
 import android.os.ParcelFileDescriptor
 import android.provider.MediaStore
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.qure.core.BaseActivity
-import com.qure.core.util.FishingMemoryToast
-import com.qure.core.util.setOnSingleClickListener
-import com.qure.core_design.custom.recyclerview.RecyclerViewItemDecoration
-import com.qure.domain.DEFAULT_GALLERY_REQUEST_CODE
+import androidx.compose.runtime.Composable
+import com.qure.core.BaseComposeActivity
+import com.qure.core_design.compose.theme.FishingMemoryTheme
 import com.qure.domain.PHOTO_FILE
-import com.qure.gallery.databinding.ActivityGalleryBinding
 import com.qure.navigator.MemoCreateNavigator
 import dagger.hilt.android.AndroidEntryPoint
-import java.io.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileDescriptor
+import java.io.FileInputStream
+import java.io.FileOutputStream
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class GalleryActivity :
-    BaseActivity<ActivityGalleryBinding>(R.layout.activity_gallery),
-    GalleryAdapter.OnItemClickListener {
+class GalleryActivity : BaseComposeActivity() {
     @Inject
     lateinit var memoCreateNavigator: MemoCreateNavigator
-
-    private lateinit var adapter: GalleryAdapter
-    private var images: MutableList<GalleryImage> = mutableListOf()
-    private var selectedImage: Uri? = null
-    private var preSelectedImage: Uri? = null
-
-    private lateinit var itemListener: GalleryAdapter.OnItemClickListener
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        itemListener = this
-        loadGalleryImages()
-        initView()
-        initRecyclerView()
-    }
-
-    private fun initView() {
-        binding.textViewActivityGalleryDone.setOnClickListener {
-            val intent = Intent()
-            intent.putExtra(PHOTO_FILE, selectedImage)
-            setResult(Activity.RESULT_OK, intent)
-            finish()
-        }
-
-        binding.imageViewActivityGalleryClose.setOnSingleClickListener {
-            finish()
+    @Composable
+    override fun Screen() {
+        FishingMemoryTheme {
+            GalleryScreen(
+                onClickClose = { finish() },
+                onClickCamera = { imageBitmap ->
+                    navigateToMemoCreate(imageBitmap)
+                },
+                onClickDone = { galleryImage ->
+                    val imageBitmap = BitmapFactory.decodeFile(galleryImage.path)
+                    navigateToMemoCreate(imageBitmap)
+                },
+            )
         }
     }
-
-    private fun initRecyclerView() {
-        adapter = GalleryAdapter(this, images, itemListener)
-        binding.recyclerViewActivityGalleryImages.adapter = adapter
-        binding.recyclerViewActivityGalleryImages.addItemDecoration(RecyclerViewItemDecoration(10))
-        adapter.submitList(images)
-    }
-
-    private fun loadGalleryImages() {
-        val externalDirs = ContextCompat.getExternalFilesDirs(this, null)
-        externalDirs.forEach { externalDir ->
-            externalDir?.let {
-                val projection = arrayOf(MediaStore.Images.Media._ID, MediaStore.Images.Media.DATA)
-                val cursor =
-                    contentResolver.query(
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                        projection,
-                        null,
-                        null,
-                        null,
-                    )
-                cursor?.let {
-                    while (it.moveToNext()) {
-                        val columnIndex = it.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-                        val idColumn = it.getColumnIndex(MediaStore.Images.Media._ID)
-                        val imagePath = it.getString(columnIndex)
-                        val id = it.getLong(idColumn)
-                        images.add(GalleryImage(id = id, path = imagePath))
-                    }
-                    it.close()
-                }
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray,
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-
-        when (requestCode) {
-            DEFAULT_GALLERY_REQUEST_CODE -> {
-                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startCamera()
-                } else {
-                    FishingMemoryToast().error(this, "권한이 거부되었습니다.")
-                }
-                return
-            }
-        }
-    }
-
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?,
-    ) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == DEFAULT_GALLERY_REQUEST_CODE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-
-            val uri = Uri.parse(getImageUri(imageBitmap).toString())
-            val file = copyUriToExternalStorage(uri)
-
-            val intent = memoCreateNavigator.intent(this)
-
-            intent.putExtra(PHOTO_FILE, Uri.fromFile(file))
-            setResult(Activity.RESULT_OK, intent)
-            finish()
-        }
+    private fun navigateToMemoCreate(imageBitmap: Bitmap) {
+        val uri = Uri.parse(getImageUri(imageBitmap).toString())
+        val file = copyUriToExternalStorage(uri)
+        val intent = memoCreateNavigator.intent(this)
+        intent.putExtra(PHOTO_FILE, Uri.fromFile(file))
+        setResult(RESULT_OK, intent)
+        finish()
     }
 
     private fun copyUriToExternalStorage(uri: Uri): File {
@@ -157,57 +73,5 @@ class GalleryActivity :
                 null,
             )
         return Uri.parse(path)
-    }
-
-    private fun startCamera() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(intent, DEFAULT_GALLERY_REQUEST_CODE)
-    }
-
-    private fun checkPermission(): Boolean {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.CAMERA,
-            ) == PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.READ_EXTERNAL_STORAGE,
-            ) == PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(
-                this,
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            ) == PackageManager.PERMISSION_GRANTED
-        ) {
-            loadGalleryImages()
-            return true
-        }
-        return false
-    }
-
-    private fun requestPermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                android.Manifest.permission.CAMERA,
-                android.Manifest.permission.READ_EXTERNAL_STORAGE,
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            ),
-            DEFAULT_GALLERY_REQUEST_CODE,
-        )
-    }
-
-    override fun setOnItemClickListener(uri: Uri) {
-        binding.textViewActivityGalleryDone.isEnabled =
-            if (preSelectedImage == uri && binding.textViewActivityGalleryDone.isEnabled) false else true
-        preSelectedImage = uri
-        selectedImage = uri
-    }
-
-    override fun setOnItemClickListener() {
-        if (checkPermission()) {
-            startCamera()
-        } else {
-            requestPermission()
-        }
     }
 }
