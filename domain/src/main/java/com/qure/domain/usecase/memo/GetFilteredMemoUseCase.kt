60d29ac424c638257d861d22982fcb53ff3b1525
dltkd1395 <dltkd13956@naver.com>
2024-05-23 17:39:22 +0900

    feat: memo 생성 날짜 내림 차순 정렬

--- domain/src/main/java/com/qure/domain/usecase/memo/GetFilteredMemoUseCase.kt
+++ domain/src/main/java/com/qure/domain/usecase/memo/GetFilteredMemoUseCase.kt
@@ -4,6 +4,7 @@ import com.qure.domain.entity.memo.Memo
 import com.qure.domain.entity.memo.MemoQuery
 import com.qure.domain.repository.MemoRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetFilteredMemoUseCase
@@ -13,5 +14,6 @@ class GetFilteredMemoUseCase
     ) {
         operator fun invoke(memoQuery: MemoQuery): Flow<List<Memo>> {
             return memoRepository.getfilteredMemo(memoQuery)
+                .map { memos -> memos.sortedByDescending { memo -> memo.createTime } }
         }
     }
