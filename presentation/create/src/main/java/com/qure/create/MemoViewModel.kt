5428f735c88c942a7b2ef1463479b643f2c8ec59
dltkd1395 <dltkd13956@naver.com>
2024-05-23 18:03:39 +0900

    refactor: memo 상태 로직 변경

--- presentation/create/src/main/java/com/qure/create/MemoViewModel.kt
+++ presentation/create/src/main/java/com/qure/create/MemoViewModel.kt
@@ -4,155 +4,184 @@ import androidx.lifecycle.viewModelScope
 import com.qure.build_property.BuildProperty
 import com.qure.build_property.BuildPropertyRepository
 import com.qure.core.BaseViewModel
-import com.qure.core.extensions.Empty
 import com.qure.core.extensions.Slash
 import com.qure.core.extensions.URLSplash
 import com.qure.core.extensions.UUID
-import com.qure.domain.entity.memo.FieldStringValue
-import com.qure.domain.entity.memo.MemoFields
+import com.qure.domain.entity.memo.Document
 import com.qure.domain.entity.memo.MemoStorage
 import com.qure.domain.repository.AuthRepository
 import com.qure.domain.usecase.memo.CreateMemoUseCase
 import com.qure.domain.usecase.memo.UpdateMemoUseCase
 import com.qure.domain.usecase.memo.UploadMemoImageUseCase
 import com.qure.memo.model.MemoUI
+import com.qure.memo.model.toMemoFields
 import com.qure.memo.model.toMemoUI
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flatMapConcat
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 import javax.inject.Inject
 
 @HiltViewModel
 class MemoViewModel
-    @Inject
-    constructor(
-        private val createMemoUseCase: CreateMemoUseCase,
-        private val uploadMemoImageUseCase: UploadMemoImageUseCase,
-        private val updateMemoUseCase: UpdateMemoUseCase,
-        private val authRepository: AuthRepository,
-        private val buildPropertyRepository: BuildPropertyRepository,
-    ) : BaseViewModel() {
-        private val _uiState = MutableStateFlow(UiState())
-        val uiState = _uiState.asStateFlow()
+@Inject
+constructor(
+    private val createMemoUseCase: CreateMemoUseCase,
+    private val uploadMemoImageUseCase: UploadMemoImageUseCase,
+    private val updateMemoUseCase: UpdateMemoUseCase,
+    private val authRepository: AuthRepository,
+    private val buildPropertyRepository: BuildPropertyRepository,
+) : BaseViewModel() {
+    private val _memoCreateUiState = MutableStateFlow<MemoCreateUiState>(MemoCreateUiState.Initial)
+    val memoCreateUiState = _memoCreateUiState.asStateFlow()
 
-        private val _memo = MutableStateFlow(MemoUI())
-        val memo = _memo.asStateFlow()
+    private val _memo = MutableStateFlow(
+        MemoUI(
+            email = authRepository.getEmailFromLocal(),
+            date = SimpleDateFormat("yyyy/MM/dd", Locale.KOREA).format(Date()),
+        )
+    )
+    val memo = _memo.asStateFlow()
 
-        fun createMemo(imageUrl: String) {
-            val memo = createMemoFields(imageUrl)
-            viewModelScope.launch {
-                createMemoUseCase(memo).onSuccess {
-                    _uiState.update {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun createMemo() {
+        viewModelScope.launch(Dispatchers.IO) {
+            uploadImageIfNeeded()
+                .onStart { _memoCreateUiState.value = MemoCreateUiState.Loading }
+                .catch { throwable -> sendErrorMessage(throwable) }
+                .flatMapConcat { imageUrl ->
+                    _memo.update {
                         it.copy(
-                            isSave = true,
+                            uuid = String.UUID,
+                            image = imageUrl,
                         )
                     }
-                }.onFailure { throwable ->
-                    throwable as Exception
-                    sendErrorMessage(throwable)
-                }
-            }
-        }
-
-        fun setMemoUI(memoUI: MemoUI) {
-            _memo.value = memoUI
-        }
-
-        private fun createMemoFields(
-            imageUrl: String,
-            uuid: String = String.UUID,
-        ): MemoFields {
-            return MemoFields(
-                uuid = FieldStringValue(uuid),
-                email = FieldStringValue(authRepository.getEmailFromLocal()),
-                title = FieldStringValue(memo.value.title),
-                image = FieldStringValue(imageUrl),
-                waterType = FieldStringValue(memo.value.waterType),
-                fishType = FieldStringValue(memo.value.fishType),
-                location = FieldStringValue(memo.value.location),
-                date = FieldStringValue(memo.value.date),
-                fishSize = FieldStringValue(memo.value.fishSize),
-                content = FieldStringValue(memo.value.content),
-                coords = FieldStringValue(uiState.value.coords),
-            )
-        }
-
-        fun uploadMemoImage(uuid: String = String.Empty) {
-            viewModelScope.launch {
-                _uiState.update {
-                    it.copy(
-                        isUploadImage = true,
+                    createMemoUseCase(
+                        memo.value.toMemoFields(authRepository.getEmailFromLocal())
                     )
+                        .onStart {
+                            _memoCreateUiState.value = MemoCreateUiState.Loading
+                        }
+                        .catch { throwable -> sendErrorMessage(throwable) }
                 }
-                uploadMemoImageUseCase(File(uiState.value.image))
-                    .onSuccess { storage ->
-                        if (uuid == String.Empty) {
-                            createMemo(getImageUrl(storage))
-                        } else {
-                            updateMemo(uuid, getImageUrl(storage))
-                        }
-                    }.onFailure { throwable ->
-                        throwable is Exception
-                        sendErrorMessage(throwable)
-                        _uiState.update {
-                            it.copy(
-                                isUploadImage = false,
-                            )
-                        }
-                    }
-            }
-        }
-
-        fun updateMemo(
-            uuid: String,
-            imageUrl: String,
-        ) {
-            val editedMemo = createMemoFields(imageUrl, uuid)
-            viewModelScope.launch {
-                updateMemoUseCase(editedMemo).collect { response ->
-                    response.onSuccess { memo ->
-                        _uiState.update {
-                            it.copy(
-                                isUpdated = true,
-                                memo = memo.toMemoUI(),
-                            )
-                        }
-                    }
+                .collectLatest {
+                    _memoCreateUiState.value = MemoCreateUiState.Success(memo.value)
                 }
-            }
-        }
-
-        private fun getImageUrl(storage: MemoStorage): String {
-            val fileName = storage.name.split(String.Slash).joinToString(String.URLSplash)
-            return buildPropertyRepository.get(BuildProperty.FIREBASE_STORAGE_URL) +
-                "o/$fileName?alt=media&token=${storage.downloadTokens}"
-        }
-
-        fun setImage(image: String) {
-            _uiState.update {
-                it.copy(
-                    image = image,
-                )
-            }
-        }
-
-        fun setCoords(coords: String) {
-            _uiState.update {
-                it.copy(
-                    coords = coords,
-                )
-            }
         }
     }
 
-data class UiState(
-    val isSave: Boolean = false,
-    val isUploadImage: Boolean = false,
-    val isUpdated: Boolean = false,
-    val memo: MemoUI? = null,
-    val image: String = String.Empty,
-    val coords: String = String.Empty,
-)
+    private fun uploadImageIfNeeded(): Flow<String> {
+        return if (_memo.value.image.startsWith("https://firebasestorage").not()) {
+            uploadMemoImageUseCase(File(_memo.value.image))
+                .flowOn(Dispatchers.IO)
+                .flatMapConcat { storage ->
+                    val imageUrl = getImageUrl(storage)
+                    _memo.update { it.copy(image = imageUrl) }
+                    flowOf(imageUrl)
+                }
+        } else {
+            flowOf(_memo.value.image)
+        }
+    }
+
+    private fun updateMemoFlow(): Flow<Document> {
+        return updateMemoUseCase(_memo.value.toMemoFields(authRepository.getEmailFromLocal()))
+    }
+
+    fun updateMemo() {
+        viewModelScope.launch {
+            uploadImageIfNeeded()
+                .onStart { _memoCreateUiState.value = MemoCreateUiState.Loading }
+                .catch { throwable ->
+                    sendErrorMessage(throwable)
+                }
+                .flatMapConcat {
+                    updateMemoFlow()
+                }
+                .collectLatest { document ->
+                    _memoCreateUiState.value = MemoCreateUiState.Success(document.toMemoUI())
+                }
+        }
+    }
+
+    fun setMemoUi(memoUI: MemoUI) {
+        _memo.update { memoUI }
+    }
+
+    private fun getImageUrl(storage: MemoStorage): String {
+        val fileName = storage.name.split(String.Slash).joinToString(String.URLSplash)
+        return buildPropertyRepository.get(BuildProperty.FIREBASE_STORAGE_URL) +
+                "o/$fileName?alt=media&token=${storage.downloadTokens}"
+    }
+
+    fun setImage(image: String) {
+        _memo.update {
+            it.copy(image = image)
+        }
+    }
+
+    fun setCoords(coords: String) {
+        _memo.update {
+            it.copy(coords = coords)
+        }
+
+    }
+
+    fun setLocation(location: String) {
+        _memo.update {
+            it.copy(location = location)
+        }
+
+    }
+
+    fun setDate(date: String) {
+        _memo.update {
+            it.copy(date = date)
+        }
+
+    }
+
+    fun setTitle(title: String) {
+        _memo.update {
+            it.copy(title = title)
+        }
+    }
+
+    fun setFishType(fishType: String) {
+        _memo.update {
+            it.copy(fishType = fishType)
+        }
+    }
+
+    fun setContent(content: String) {
+        _memo.update {
+            it.copy(content = content)
+        }
+    }
+
+    fun setSize(size: String) {
+        _memo.update {
+            it.copy(fishSize = size)
+        }
+    }
+
+    fun setWaterType(waterType: String) {
+        _memo.update {
+            it.copy(waterType = waterType)
+        }
+    }
+}
