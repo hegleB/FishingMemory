b85554887f5c23b4c07101d414d4f1d51d284e8f
dltkd1395 <dltkd13956@naver.com>
2024-05-24 18:12:21 +0900

    refactor: login 상태 로직 변경

--- presentation/login/src/main/java/com/qure/login/LoginViewModel.kt
+++ presentation/login/src/main/java/com/qure/login/LoginViewModel.kt
@@ -2,17 +2,15 @@ package com.qure.login
 
 import androidx.lifecycle.viewModelScope
 import com.qure.core.BaseViewModel
-import com.qure.core.extensions.Colon
-import com.qure.data.datasource.FishMemorySharedPreference
-import com.qure.domain.ACCESS_TOKEN_KEY
-import com.qure.domain.entity.auth.SignUpUser
 import com.qure.domain.repository.AuthRepository
 import com.qure.domain.usecase.auth.CreateUserUseCase
 import com.qure.domain.usecase.auth.GetUserTokenUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
-import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -22,76 +20,58 @@ class LoginViewModel
 constructor(
     private val createUserUseCase: CreateUserUseCase,
     private val getUserTokenUseCase: GetUserTokenUseCase,
-    private val fishingSharedPreference: FishMemorySharedPreference,
     private val authRepository: AuthRepository,
 ) : BaseViewModel() {
-    private val _action = MutableSharedFlow<Action>()
-    val action: SharedFlow<Action>
-        get() = _action.asSharedFlow()
+
+    private val _loginUiState = MutableStateFlow<LoginUiState>(LoginUiState.Initial)
+    val loginUiState = _loginUiState.asStateFlow()
 
     fun createUser(
         email: String,
         accessToken: String,
     ) = viewModelScope.launch {
-        kotlin.runCatching {
-            createUserUseCase(
-                email = email,
-                socialToken = accessToken,
-            ).onSuccess { value: SignUpUser ->
-                _action.emit(Action.FirstSignUp)
-            }.onFailure { throwable ->
+        createUserUseCase(email, accessToken)
+            .onStart { _loginUiState.value = LoginUiState.Loading }
+            .catch { throwable ->
                 if (isExistsEmail(throwable.message)) {
-                    saveToLocalSignedUpUser(email)
-                    _action.emit(Action.AlreadySignUp)
+                    handleExistingUser(email, accessToken)
                 } else {
-                    sendErrorMessage(throwable.message)
+                    sendErrorMessage(throwable)
                 }
+            }.collectLatest {
+                authRepository.saveEmailToLocal(email)
+                authRepository.saveTokenToLocal(accessToken)
+                _loginUiState.value = LoginUiState.Success
             }
-        }.onFailure {
-            it as Exception
-            if (isExistsEmail(it.message)) {
-                saveToLocalSignedUpUser(email)
-                _action.emit(Action.AlreadySignUp)
-            } else {
-                sendErrorMessage(it.message)
-            }
-        }
     }
 
-    private fun saveToLocalSignedUpUser(email: String) =
-        viewModelScope.launch {
-            authRepository.saveEmailToLocal(email)
-            getUserTokenUseCase(email = email).collect { response ->
-                response.onSuccess { user ->
-                    fishingSharedPreference.putString(
-                        ACCESS_TOKEN_KEY,
-                        user.fields.token.stringValue,
-                    )
-                }.onFailure { throwable ->
-                    throwable as Exception
-                    sendErrorMessage(throwable.message)
-                }
+    private suspend fun handleExistingUser(
+        email: String,
+        accessToken: String,
+    ) {
+        getUserTokenUseCase(email)
+            .onStart { _loginUiState.value = LoginUiState.Loading }
+            .catch { throwable ->
+                sendErrorMessage(throwable)
             }
-        }
+            .collectLatest {
+                authRepository.saveEmailToLocal(email)
+                authRepository.saveTokenToLocal(accessToken)
+                _loginUiState.value = LoginUiState.Success
+            }
+    }
 
     fun onClickedKakaoLogin() {
         viewModelScope.launch {
-            _action.emit(Action.LauchKakaoLogin)
+            _loginUiState.value = LoginUiState.LaunchLogin
         }
     }
 
     private fun isExistsEmail(message: String?): Boolean {
-        val documentMessage = message?.split(String.Colon) ?: emptyList()
-        return documentMessage[0] == EMAIL_EXISTS
-    }
-
-    sealed class Action {
-        object FirstSignUp : Action()
-        object AlreadySignUp : Action()
-        object LauchKakaoLogin : Action()
+        return message?.contains(CONFLICT_CODE) == true
     }
 
     companion object {
-        private const val EMAIL_EXISTS = "Document already exists"
+        private const val CONFLICT_CODE = "HTTP 409"
     }
 }
