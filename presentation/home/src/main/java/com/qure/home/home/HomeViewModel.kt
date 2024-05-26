be90a70615997678483f255f33317658afe57121
dltkd1395 <dltkd13956@naver.com>
2024-05-26 16:34:31 +0900

    refactor: home 상태 로직 변경

--- presentation/home/src/main/java/com/qure/home/home/HomeViewModel.kt
+++ presentation/home/src/main/java/com/qure/home/home/HomeViewModel.kt
@@ -5,19 +5,29 @@ import com.qure.core.BaseViewModel
 import com.qure.core.extensions.DefaultLatitude
 import com.qure.core.extensions.DefaultLongitude
 import com.qure.core.extensions.twoDigitsFormat
-import com.qure.domain.entity.memo.*
-import com.qure.domain.entity.weather.WeatherCategory
+import com.qure.domain.entity.memo.CollectionId
+import com.qure.domain.entity.memo.CompositeFilter
+import com.qure.domain.entity.memo.FieldFilter
+import com.qure.domain.entity.memo.FieldPath
+import com.qure.domain.entity.memo.Filter
+import com.qure.domain.entity.memo.MemoQuery
+import com.qure.domain.entity.memo.OrderBy
+import com.qure.domain.entity.memo.StructuredQuery
+import com.qure.domain.entity.memo.Value
+import com.qure.domain.entity.memo.Where
 import com.qure.domain.repository.AuthRepository
 import com.qure.domain.usecase.memo.GetFilteredMemoUseCase
 import com.qure.domain.usecase.weather.GetWeatherUseCase
-import com.qure.home.home.model.WeatherUI
 import com.qure.home.home.model.toWeatherUI
-import com.qure.memo.model.MemoUI
 import com.qure.memo.model.toMemoUI
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.LocalTime
@@ -26,153 +36,107 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel
-    @Inject
-    constructor(
-        private val getWeatherUseCase: GetWeatherUseCase,
-        private val getFilteredMemoUseCase: GetFilteredMemoUseCase,
-        private val authRepository: AuthRepository,
-    ) : BaseViewModel() {
-        private val _UiState: MutableStateFlow<UiState> = MutableStateFlow(UiState())
-        val UiState: StateFlow<UiState>
-            get() = _UiState
+@Inject
+constructor(
+    private val getWeatherUseCase: GetWeatherUseCase,
+    private val getFilteredMemoUseCase: GetFilteredMemoUseCase,
+    private val authRepository: AuthRepository,
+) : BaseViewModel() {
 
-        private val _checkedId: MutableStateFlow<Int> = MutableStateFlow(-1)
-        val checkedId: StateFlow<Int>
-            get() = _checkedId
-
-        fun fetchWeater(latXLngY: LatXLngY) {
-            viewModelScope.launch {
-                getWeatherUseCase(
-                    base_date = getBaseDate(),
-                    base_time = getBaseTime(),
-                    nx = latXLngY.nx.toInt().toString(),
-                    ny = latXLngY.ny.toInt().toString(),
-                ).collect { response ->
-                    response.onSuccess { weather ->
-                        if (weather.response.body != null) {
-                            _UiState.update {
-                                it.copy(
-                                    weatherUI = weather.response.body.items.item.map { it.toWeatherUI() },
-                                    isWeatherInitialized = true,
-                                    latXLngY = latXLngY,
-                                )
-                            }
-                        }
-                    }.onFailure {
-                        sendErrorMessage(it.message)
-                    }
-                }
-            }
-        }
-
-        fun getFilteredMemo() {
-            viewModelScope.launch {
-                getFilteredMemoUseCase(
-                    getStructuredQuery(),
-                ).collect { response ->
-                    response.onSuccess { result ->
-                        _UiState.update {
-                            it.copy(
-                                isFilterInitialized = true,
-                                filteredMemo = result.map { it.toMemoUI() },
-                            )
-                        }
-                    }.onFailure { throwable ->
-                        sendErrorMessage(throwable)
-                    }
-                }
-            }
-        }
-
-        fun setCheckedId(checkedId: Int) {
-            _UiState.update {
-                it.copy(
-                    checkedId = checkedId,
-                )
-            }
-        }
-
-        private fun getStructuredQuery(): MemoQuery {
-            val emailFilter =
-                FieldFilter(
-                    field = FieldPath(EMAIL),
-                    op = EQUAL,
-                    value = Value(authRepository.getEmailFromLocal()),
-                )
-
-            val compositeFilter =
-                CompositeFilter(
-                    op = AND,
-                    filters = listOf(Filter(emailFilter)),
-                )
-
-            return MemoQuery(
-                StructuredQuery(
-                    from = listOf(CollectionId(COLLECTION_ID)),
-                    where = Where(compositeFilter),
-                    orderBy = listOf(OrderBy(FieldPath(DATE), DESCENDING)),
-                ),
-            )
-        }
-
-        private fun getBaseTime(): String {
-            val baseTime = "${(LocalTime.now().hour - 1).twoDigitsFormat()}30"
-            return baseTime
-        }
-
-        private fun getBaseDate(): Int {
-            val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
-            val baseDate = LocalDate.now().format(formatter).toInt()
-            return baseDate
-        }
-
-        companion object {
-            private const val EMAIL = "email"
-            private const val DATE = "date"
-            private const val DESCENDING = "DESCENDING"
-            private const val EQUAL = "EQUAL"
-            private const val AND = "AND"
-            private const val COLLECTION_ID = "memo"
-        }
-    }
-
-data class UiState(
-    val weatherUI: List<WeatherUI>? = null,
-    val isWeatherInitialized: Boolean = false,
-    val isFilterInitialized: Boolean = false,
-    val filteredMemo: List<MemoUI> = emptyList(),
-    val checkedId: Int = -1,
-    val latXLngY: LatXLngY =
+    private val _selectedChip = MutableStateFlow(INITIAL_FISH_TYPE)
+    val selectedChip = _selectedChip.asStateFlow()
+    private val _latLng = MutableStateFlow(
         LatXLngY(
             String.DefaultLatitude.toDouble(),
             String.DefaultLongitude.toDouble(),
-        ),
-) {
-    fun toTemperatureString(): String {
-        return "${getTemperatureState().checkedWeather}°"
+        )
+    )
+    val latLng = _latLng.asStateFlow()
+
+    private val _homeUiState = MutableStateFlow<HomeUiState>(HomeUiState.Loading)
+    val homeUiState = _homeUiState.asStateFlow()
+
+    init {
+        fetchData()
     }
 
-    fun toWeatherAnimation(): Int {
-        when {
-            getThunder().fcstValue.toInt() > 1 -> return getThunder().checkedWeather
-            getPrecipitationState().fcstValue.toInt() > 0 -> return getPrecipitationState().checkedWeather
-            else -> return getSkyState().checkedWeather
+    fun fetchData() {
+        viewModelScope.launch {
+            combine(
+                getWeatherUseCase(
+                    base_date = getBaseDate(),
+                    base_time = getBaseTime(),
+                    nx = _latLng.value.nx.toInt().toString(),
+                    ny = _latLng.value.ny.toInt().toString(),
+                ),
+                getFilteredMemoUseCase(
+                    getStructuredQuery(),
+                ),
+                ::Pair
+            )
+                .map { ui ->
+                    HomeUiState.Success(
+                        weather = ui.first.response.body.items.item.map { it.toWeatherUI() },
+                        memos = ui.second.map { it.toMemoUI() }
+                    )
+                }
+                .onStart { _homeUiState.value = HomeUiState.Loading }
+                .catch { throwable -> sendErrorMessage(throwable) }
+                .collectLatest { homeUiState ->
+                    _homeUiState.value = homeUiState
+                }
         }
     }
 
-    fun getTemperatureState(): WeatherUI {
-        return weatherUI?.filter { it.category == WeatherCategory.T1H }?.get(0) ?: WeatherUI()
+
+    fun setSelectedChip(chip: String) {
+        _selectedChip.value = chip
     }
 
-    fun getSkyState(): WeatherUI {
-        return weatherUI?.filter { it.category == WeatherCategory.SKY }?.get(0) ?: WeatherUI()
+    fun setLatLng(latXLngY: LatXLngY) {
+        _latLng.value = latXLngY
     }
 
-    fun getPrecipitationState(): WeatherUI {
-        return weatherUI?.filter { it.category == WeatherCategory.PTY }?.get(0) ?: WeatherUI()
+    private fun getStructuredQuery(): MemoQuery {
+        val emailFilter =
+            FieldFilter(
+                field = FieldPath(EMAIL),
+                op = EQUAL,
+                value = Value(authRepository.getEmailFromLocal()),
+            )
+
+        val compositeFilter =
+            CompositeFilter(
+                op = AND,
+                filters = listOf(Filter(emailFilter)),
+            )
+
+        return MemoQuery(
+            StructuredQuery(
+                from = listOf(CollectionId(COLLECTION_ID)),
+                where = Where(compositeFilter),
+                orderBy = listOf(OrderBy(FieldPath(DATE), DESCENDING)),
+            ),
+        )
+    }
+    private fun getBaseTime(): String {
+        val baseTime = "${(LocalTime.now().hour - 1).twoDigitsFormat()}30"
+        return baseTime
     }
 
-    fun getThunder(): WeatherUI {
-        return weatherUI?.filter { it.category == WeatherCategory.LGT }?.get(0) ?: WeatherUI()
+    private fun getBaseDate(): Int {
+        val formatter = DateTimeFormatter.ofPattern("yyyyMMdd")
+        val baseDate = LocalDate.now().format(formatter).toInt()
+        return baseDate
+    }
+
+    companion object {
+        private const val EMAIL = "email"
+        private const val DATE = "date"
+        private const val DESCENDING = "DESCENDING"
+        private const val EQUAL = "EQUAL"
+        private const val AND = "AND"
+        private const val COLLECTION_ID = "memo"
+        private const val INITIAL_FISH_TYPE = "어종"
     }
 }
