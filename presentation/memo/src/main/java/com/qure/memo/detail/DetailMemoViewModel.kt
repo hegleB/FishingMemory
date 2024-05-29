1078ff87b44aa6882506f68253534dd2535a0cab
dltkd1395 <dltkd13956@naver.com>
2024-05-30 00:41:59 +0900

    refactor: 상태 로직 변경

--- presentation/memo/src/main/java/com/qure/memo/detail/DetailMemoViewModel.kt
+++ presentation/memo/src/main/java/com/qure/memo/detail/DetailMemoViewModel.kt
@@ -6,37 +6,29 @@ import com.qure.core.extensions.Empty
 import com.qure.domain.usecase.memo.DeleteMemoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DetailMemoViewModel
-    @Inject
-    constructor(
-        private val deleteMemoUseCase: DeleteMemoUseCase,
-    ) : BaseViewModel() {
-        private val _uiState = MutableStateFlow(UiState())
-        val uiState: StateFlow<UiState>
-            get() = _uiState
+@Inject
+constructor(
+    private val deleteMemoUseCase: DeleteMemoUseCase,
+) : BaseViewModel() {
+    private val _detailMemoUiState = MutableStateFlow<DetailMemoUiState>(DetailMemoUiState.Initial)
+    val detailMemoUiState = _detailMemoUiState.asStateFlow()
 
-        fun deleteMemo(uuid: String) {
-            viewModelScope.launch {
-                deleteMemoUseCase(uuid).collect { response ->
-                    response.onSuccess {
-                        _uiState.update {
-                            it.copy(
-                                isDeleteInitialized = true,
-                                deleteSuccessMessage = DELETE_SUCCESS_MESSAGE,
-                            )
-                        }
-                    }.onFailure { throwable ->
-                        sendErrorMessage(throwable)
-                    }
+    fun deleteMemo(uuid: String) {
+        viewModelScope.launch {
+            deleteMemoUseCase(uuid)
+                .onStart { DetailMemoUiState.Loading }
+                .catch { throwable -> sendErrorMessage(throwable) }
+                .collect {
+                    _detailMemoUiState.value = DetailMemoUiState.Success
                 }
-            }
-        }
 
         companion object {
             const val DELETE_SUCCESS_MESSAGE = "삭제가 완료되었습니다"
