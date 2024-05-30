bdfd01ab815f77a04505c8347974a26ebf003544
dltkd1395 <dltkd13956@naver.com>
2024-05-30 18:41:29 +0900

    refactor: 불필요한 import 제거

--- presentation/fishingspot/src/main/java/com/qure/fishingspot/FishingSpotScreen.kt
+++ presentation/fishingspot/src/main/java/com/qure/fishingspot/FishingSpotScreen.kt
@@ -1,33 +1,20 @@
 package com.qure.fishingspot
 
-import androidx.annotation.DrawableRes
-import androidx.annotation.StringRes
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,7 +28,6 @@ import com.qure.core_design.compose.components.FMBackButton
 import com.qure.core_design.compose.components.FMBookmarkButton
 import com.qure.core_design.compose.components.FMFishingSpotItem
 import com.qure.core_design.compose.components.FMNaverMap
-import com.qure.core_design.compose.theme.Gray300
 import com.qure.core_design.compose.utils.FMPreview
 import com.qure.model.FishingSpotUI
 
