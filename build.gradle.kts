28328bd7492aece0debb6dbd093bc22d768cffa9
dltkd1395 <dltkd13956@naver.com>
2024-06-03 22:32:57 +0900

    chore: serialization, ksp, compose plugin 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,20 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+//// Top-level build file where you can add configuration options common to all sub-projects/modules.
+//
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
 
 plugins {
-    alias(libs.plugins.com.android.application) apply false
-    alias(libs.plugins.com.android.library) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.jvm) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.hilt) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
