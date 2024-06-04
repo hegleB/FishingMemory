b2426fff0e37a771e6d32b62dc5a28b5f44bd6c4
dltkd1395 <dltkd13956@naver.com>
2024-06-04 23:40:07 +0900

    feat: android,  dependencies 불필요한 코드 제거

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,28 @@
+import com.fishingmemory.app.setNamespace
+
+plugins {
+    id("fishingmemory.android.library")
+    id("fishingmemory.android.compose")
+    id("kotlinx-serialization")
+}
+
+android {
+    setNamespace("core.ui")
+}
+
+dependencies {
+    implementation(project(":core:model"))
+    implementation(project(":core:designsystem"))
+
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.navermap.sdk)
+    implementation(libs.navermap.clustering)
+    implementation(libs.calendar)
+    implementation(libs.chart)
+    implementation(libs.jetpack.material)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.viewModelCompose)
+    implementation(libs.androidx.lifecycle.runtimeCompose)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.constraintlayout)
+}
