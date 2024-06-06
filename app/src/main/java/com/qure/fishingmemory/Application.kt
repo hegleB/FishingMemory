39302126dfc94889e871b3451b36c2c61465dcbe
dltkd1395 <dltkd13956@naver.com>
2024-06-06 23:24:35 +0900

    refactor: 불필요한 코드 제거

--- app/src/main/java/com/qure/fishingmemory/Application.kt
+++ app/src/main/java/com/qure/fishingmemory/Application.kt
@@ -1,15 +1,10 @@
 package com.qure.fishingmemory
 
 import android.app.Application
-import androidx.appcompat.app.AppCompatDelegate
 import com.kakao.sdk.common.KakaoSdk
 import com.naver.maps.map.NaverMapSdk
 import com.qure.build_property.BuildProperty
 import com.qure.build_property.BuildPropertyRepository
-import com.qure.data.datasource.FishMemorySharedPreference
-import com.qure.domain.DARK_MODE_KEY
-import com.qure.domain.THEME_DARK
-import com.qure.domain.THEME_LIGHT
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import javax.inject.Inject
@@ -18,29 +13,14 @@ import javax.inject.Inject
 class Application : Application() {
     @Inject
     lateinit var buildPropertyRepository: BuildPropertyRepository
-
-    @Inject
-    lateinit var fishMemorySharedPreference: FishMemorySharedPreference
-
     override fun onCreate() {
         super.onCreate()
 
         initTimber()
         initKakaoSdk()
         initNaverMapSdk()
-        initDarkMode()
     }
 
-    private fun initDarkMode() {
-        val isDarkMode =
-            when (fishMemorySharedPreference.getTheme(DARK_MODE_KEY)) {
-                THEME_DARK -> AppCompatDelegate.MODE_NIGHT_YES
-                THEME_LIGHT -> AppCompatDelegate.MODE_NIGHT_NO
-                else -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
-            }
-
-        AppCompatDelegate.setDefaultNightMode(isDarkMode)
-    }
 
     private fun initNaverMapSdk() {
         NaverMapSdk.getInstance(this).setClient(
