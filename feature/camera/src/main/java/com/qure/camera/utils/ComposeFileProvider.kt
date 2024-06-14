5c194b833855105052aaaf6ab8067d107bebd9f1
dltkd1395 <dltkd13956@naver.com>
2024-06-14 23:47:39 +0900

    refactor: ComposeFileProvider comporess 추가 및 camera 모듈 이동

--- feature/camera/src/main/java/com/qure/camera/utils/ComposeFileProvider.kt
+++ feature/camera/src/main/java/com/qure/camera/utils/ComposeFileProvider.kt
@@ -1,16 +1,19 @@
-package com.qure.gallery
+package com.qure.camera.utils
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.core.content.FileProvider
-import com.qure.feature.gallery.R
+import com.qure.feature.camera.R
+
 import java.io.File
+import java.io.FileOutputStream
 
 class ComposeFileProvider : FileProvider(
     R.xml.file_paths,
 ) {
     companion object {
-        fun getImageUri(context: Context): Uri {
+        fun getImageUri(context: Context, bitmap: Bitmap): Uri {
             val directory = File(context.cacheDir, "images")
             directory.mkdirs()
             val file = File.createTempFile(
@@ -18,6 +21,10 @@ class ComposeFileProvider : FileProvider(
                 ".jpg",
                 directory,
             )
+            val stream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
             val authority = context.packageName + ".provider"
             return getUriForFile(
                 context,
