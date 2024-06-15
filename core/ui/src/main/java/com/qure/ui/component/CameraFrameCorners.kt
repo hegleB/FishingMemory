6c571ce0a4a74119fbf1db7cf22d22783b36872d
dltkd1395 <dltkd13956@naver.com>
2024-06-15 23:59:49 +0900

    fix: 프레임 테두리 화면에 맞게 수정

--- core/ui/src/main/java/com/qure/ui/component/CameraFrameCorners.kt
+++ core/ui/src/main/java/com/qure/ui/component/CameraFrameCorners.kt
@@ -6,9 +6,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.dp
+import com.qure.designsystem.theme.White
 
 @Composable
 fun CameraFrameCorners(
@@ -18,93 +18,134 @@ fun CameraFrameCorners(
         modifier = modifier
             .fillMaxSize()
     ) {
-        drawAllCorners(
-            width = size.width,
-            height = size.height,
-            cornerRadius = 50f,
-            lineStrokeWidth = 15f,
+        val width = size.width * 0.55f
+        val height = size.height * 0.4f
+        val cornerRadius = 30.dp.toPx()
+
+        val topLeftCenterX = width * 0.25f
+        val topLeftCenterY = height * 0.25f
+
+        drawArc(
+            color = White,
+            startAngle = 180f,
+            sweepAngle = 90f,
+            useCenter = false,
+            topLeft = Offset(topLeftCenterX, topLeftCenterY),
+            size = Size(cornerRadius * 2, cornerRadius * 2),
+            style = Stroke(width = 15f)
+        )
+
+        drawLine(
+            color = White,
+            start = Offset(x = topLeftCenterX + cornerRadius, y = topLeftCenterY),
+            end = Offset(x = topLeftCenterX + cornerRadius * 2, y = topLeftCenterY),
+            strokeWidth = 15f
+        )
+
+        drawLine(
+            color = White,
+            start = Offset(x = topLeftCenterX, y = topLeftCenterY + cornerRadius),
+            end = Offset(x = topLeftCenterX, y = topLeftCenterY + cornerRadius * 2),
+            strokeWidth = 15f
+        )
+
+        val topRightCenterX = width * 1.5f
+        val topRightCenterY = height * 0.25f
+
+        drawArc(
+            color = White,
+            startAngle = 270f,
+            sweepAngle = 90f,
+            useCenter = false,
+            topLeft = Offset(topRightCenterX, topRightCenterY),
+            size = Size(cornerRadius * 2, cornerRadius * 2),
+            style = Stroke(width = 15f)
+        )
+
+        drawLine(
+            color = White,
+            start = Offset(x = topRightCenterX, y = topRightCenterY),
+            end = Offset(x = topRightCenterX + cornerRadius, y = topRightCenterY),
+            strokeWidth = 15f
+        )
+
+        drawLine(
+            color = White,
+            start = Offset(
+                x = topRightCenterX + cornerRadius * 2,
+                y = topRightCenterY + cornerRadius
+            ),
+            end = Offset(
+                x = topRightCenterX + cornerRadius * 2,
+                y = topRightCenterY + cornerRadius * 2
+            ),
+            strokeWidth = 15f
+        )
+
+        val bottomLeftCenterX = width * 0.25f
+        val bottomLeftCenterY = height * 1.5f
+
+        drawArc(
+            color = White,
+            startAngle = 90f,
+            sweepAngle = 90f,
+            useCenter = false,
+            topLeft = Offset(bottomLeftCenterX, bottomLeftCenterY),
+            size = Size(cornerRadius * 2, cornerRadius * 2),
+            style = Stroke(width = 15f)
+        )
+
+        drawLine(
+            color = White,
+            start = Offset(
+                x = bottomLeftCenterX + cornerRadius * 2,
+                y = bottomLeftCenterY + cornerRadius * 2
+            ),
+            end = Offset(
+                x = bottomLeftCenterX + cornerRadius,
+                y = bottomLeftCenterY + cornerRadius * 2
+            ),
+            strokeWidth = 15f
+        )
+
+        drawLine(
+            color = White,
+            start = Offset(x = bottomLeftCenterX, y = bottomLeftCenterY + cornerRadius),
+            end = Offset(x = bottomLeftCenterX, y = bottomLeftCenterY),
+            strokeWidth = 15f
+        )
+
+        val bottomRightCenterX = width * 1.5f
+        val bottomRightCenterY = height * 1.5f
+
+        drawArc(
+            color = White,
+            startAngle = 0f,
+            sweepAngle = 90f,
+            useCenter = false,
+            topLeft = Offset(bottomRightCenterX, bottomRightCenterY),
+            size = Size(cornerRadius * 2, cornerRadius * 2),
+            style = Stroke(width = 15f)
+        )
+
+        drawLine(
+            color = White,
+            start = Offset(
+                x = bottomRightCenterX + cornerRadius * 2,
+                y = bottomRightCenterY + cornerRadius
+            ),
+            end = Offset(x = bottomRightCenterX + cornerRadius * 2, y = bottomRightCenterY),
+            strokeWidth = 15f
+        )
+
+        drawLine(
+            color = White,
+            start = Offset(
+                x = bottomRightCenterX + cornerRadius,
+                y = bottomRightCenterY + cornerRadius * 2
+            ),
+            end = Offset(x = bottomRightCenterX, y = bottomRightCenterY + cornerRadius * 2),
+            strokeWidth = 15f
         )
     }
-}
-fun DrawScope.drawCorner(
-    color: Color,
-    centerX: Float,
-    centerY: Float,
-    startAngle: Float,
-    sweepAngle: Float,
-    cornerRadius: Float,
-    lineStrokeWidth: Float
-) {
-    drawArc(
-        color = color,
-        startAngle = startAngle,
-        sweepAngle = sweepAngle,
-        useCenter = false,
-        topLeft = Offset(centerX, centerY),
-        size = Size(cornerRadius * 2, cornerRadius * 2),
-        style = Stroke(width = lineStrokeWidth)
-    )
-
-    drawLine(
-        color = color,
-        start = Offset(x = centerX + cornerRadius, y = centerY),
-        end = Offset(x = centerX + cornerRadius * 2, y = centerY),
-        strokeWidth = lineStrokeWidth
-    )
-
-    drawLine(
-        color = color,
-        start = Offset(x = centerX, y = centerY + cornerRadius),
-        end = Offset(x = centerX, y = centerY + cornerRadius * 2),
-        strokeWidth = lineStrokeWidth
-    )
-}
-
-fun DrawScope.drawAllCorners(
-    width: Float,
-    height: Float,
-    cornerRadius: Float,
-    lineStrokeWidth: Float
-) {
-    val white = Color.White
-
-    drawCorner(
-        color = white,
-        centerX = width * 0.25f,
-        centerY = height * 0.25f,
-        startAngle = 180f,
-        sweepAngle = 90f,
-        cornerRadius = cornerRadius,
-        lineStrokeWidth = lineStrokeWidth
-    )
-
-    drawCorner(
-        color = white,
-        centerX = width * 1.5f,
-        centerY = height * 0.25f,
-        startAngle = 270f,
-        sweepAngle = 90f,
-        cornerRadius = cornerRadius,
-        lineStrokeWidth = lineStrokeWidth
-    )
-
-    drawCorner(
-        color = white,
-        centerX = width * 0.25f,
-        centerY = height * 1.5f,
-        startAngle = 90f,
-        sweepAngle = 90f,
-        cornerRadius = cornerRadius,
-        lineStrokeWidth = lineStrokeWidth
-    )
-
-    drawCorner(
-        color = white,
-        centerX = width * 1.5f,
-        centerY = height * 1.5f,
-        startAngle = 0f,
-        sweepAngle = 90f,
-        cornerRadius = cornerRadius,
-        lineStrokeWidth = lineStrokeWidth
-    )
 }
