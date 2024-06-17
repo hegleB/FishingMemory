38e29c805c5b47061e37f2e98609c28d679eb673
dltkd1395 <dltkd13956@naver.com>
2024-06-18 01:16:12 +0900

    feat: guide line 추가

--- feature/camera/src/main/java/com/qure/camera/CameraScreen.kt
+++ feature/camera/src/main/java/com/qure/camera/CameraScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -165,8 +166,15 @@ fun CameraScreen(
                 painter = painterResource(com.qure.core.designsystem.R.drawable.ic_fish),
                 contentDescription = null,
             )
-
-            Spacer(modifier = Modifier.height(100.dp))
+            Spacer(
+                modifier = Modifier.height(50.dp)
+            )
+            HorizontalDivider(
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
+            Spacer(
+                modifier = Modifier.height(50.dp)
+            )
             Image(
                 modifier = Modifier
                     .size(120.dp),
