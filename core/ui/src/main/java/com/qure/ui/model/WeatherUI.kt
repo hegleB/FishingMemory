49e172bdbe19cbae8fdbb45a170b0f5870db0cce
dltkd1395 <dltkd13956@naver.com>
2024-06-23 19:23:07 +0900

    refactor: HomeUiState 클래스의 날씨 관련 로직을 WeatherUI 클래스로 이동

--- core/ui/src/main/java/com/qure/ui/model/WeatherUI.kt
+++ core/ui/src/main/java/com/qure/ui/model/WeatherUI.kt
@@ -1,5 +1,6 @@
 package com.qure.ui.model
 
+import android.annotation.SuppressLint
 import com.qure.core.designsystem.R
 import com.qure.model.weather.Item
 import com.qure.model.weather.WeatherCategory
@@ -18,6 +19,65 @@ data class WeatherUI(
     val checkedWeather: Int = if (isDayTime()) R.raw.weather_sunny_day else R.raw.weather_sunny_night,
 )
 
+data class HourOfWeatherState(
+    val skyState: WeatherUI,
+    val temperature: WeatherUI,
+    val precipitationState: WeatherUI,
+    val thunder: WeatherUI,
+) {
+
+    fun toTemperatureString(): String {
+        return "${temperature.checkedWeather}°"
+    }
+    fun toWeatherAnimation(): Int {
+        return when {
+            thunder.fcstValue.toInt() > 1 -> thunder.checkedWeather
+            precipitationState.fcstValue.toInt() > 0 -> precipitationState.checkedWeather
+            else -> skyState.checkedWeather
+        }
+    }
+
+    fun convertToHourString(): String {
+        val hour = this.temperature.fcstTime / 100
+        return String.format("%d시", hour)
+    }
+
+    fun toCurrentDateTimeWeather(): String {
+        return "${convertToDate(this.temperature.fcstDate)}\n${convertToAmPm(this.temperature.fcstTime)} 기준"
+    }
+
+    private fun convertToDate(date: Int): String {
+        val month = (date % 10000) / 100
+        val day = date % 100
+        return String.format("%02d/%02d", month, day)
+    }
+
+    private fun convertToAmPm(time: Int): String {
+        val hour = time / 100
+        val minute = time % 100
+        val period = if (hour >= 12) "오후" else "오전"
+        val adjustedHour = if (hour % 12 == 0) 12 else hour % 12
+        return String.format("%s %d:%02d", period, adjustedHour, minute)
+    }
+}
+
+fun List<WeatherUI>.categorizeWeather(): List<HourOfWeatherState> {
+    if (isEmpty()) return emptyList()
+    val skyStates = this.filter { it.category == WeatherCategory.SKY }
+    val temperatures = this.filter { it.category == WeatherCategory.T1H }
+    val precipitationStates = this.filter { it.category == WeatherCategory.PTY }
+    val thunders = this.filter { it.category == WeatherCategory.LGT }
+    val minSize = minOf(skyStates.size, temperatures.size, precipitationStates.size, thunders.size)
+    return List(minSize.minus(1)) { index ->
+        HourOfWeatherState(
+            skyState = skyStates[index],
+            temperature = temperatures[index],
+            precipitationState = precipitationStates[index],
+            thunder = thunders[index]
+        )
+    }
+}
+
 fun Item.toWeatherUI(): WeatherUI {
     return WeatherUI(
         baseDate = this.baseDate,
@@ -29,19 +89,24 @@ fun Item.toWeatherUI(): WeatherUI {
         nx = this.nx,
         ny = this.ny,
         checkedWeather =
-            when (this.category) {
-                WeatherCategory.T1H -> this.fcstValue.toInt()
-                WeatherCategory.LGT -> R.raw.weather_thunder
-                WeatherCategory.SKY -> getSky(this.fcstValue.toInt())
-                WeatherCategory.PTY -> getPrecipitationState(this.fcstValue.toInt())
-                else -> 0
-            },
+        when (this.category) {
+            WeatherCategory.T1H -> this.fcstValue.toInt()
+            WeatherCategory.LGT -> R.raw.weather_thunder
+            WeatherCategory.SKY -> getSky(this.fcstValue.toInt())
+            WeatherCategory.PTY -> getPrecipitationState(this.fcstValue.toInt())
+            else -> 0
+        },
     )
 }
 
 fun getPrecipitationState(pty: Int): Int {
     return when (pty) {
-        in listOf(1, 2, 5) -> if (isDayTime()) R.raw.weather_rainy_day else R.raw.weather_rainy_night
+        in listOf(
+            1,
+            2,
+            5
+        ) -> if (isDayTime()) R.raw.weather_rainy_day else R.raw.weather_rainy_night
+
         in listOf(3, 6, 7) -> if (isDayTime()) R.raw.weather_snow_day else R.raw.weather_snow_night
         else -> 0
     }
@@ -49,12 +114,13 @@ fun getPrecipitationState(pty: Int): Int {
 
 private fun getSky(sky: Int): Int {
     return when (sky) {
-        SKY_STATE.SUNNY.nubmer -> if (isDayTime()) R.raw.weather_sunny_day else R.raw.weather_sunny_night
-        SKY_STATE.CLOUDY.nubmer -> if (isDayTime()) R.raw.weather_partly_cloudy_day else R.raw.weather_partly_cloudy_night
+        SkyState.SUNNY.number -> if (isDayTime()) R.raw.weather_sunny_day else R.raw.weather_sunny_night
+        SkyState.CLOUDY.number -> if (isDayTime()) R.raw.weather_partly_cloudy_day else R.raw.weather_partly_cloudy_night
         else -> R.raw.weather_cloudey
     }
 }
 
+@SuppressLint("SimpleDateFormat")
 private fun isDayTime(): Boolean {
     val now = System.currentTimeMillis()
     val currentTime = Date(now)
@@ -62,7 +128,7 @@ private fun isDayTime(): Boolean {
     return formatTime.format(currentTime).toInt() in 6..17
 }
 
-enum class SKY_STATE(val nubmer: Int) {
+enum class SkyState(val number: Int) {
     SUNNY(1),
     CLOUDY(3),
 }
--- feature/home/src/main/java/com/qure/home/HomeUiState.kt
+++ feature/home/src/main/java/com/qure/home/HomeUiState.kt
@@ -1,57 +1,11 @@
 package com.qure.home
 
-import com.qure.model.weather.WeatherCategory
+import com.qure.ui.model.WeatherUI
 
 sealed interface HomeUiState {
     data object Loading : HomeUiState
-    data object Empty : HomeUiState
     data class Success(
-        val weather: List<com.qure.ui.model.WeatherUI> = emptyList(),
+        val weather: List<WeatherUI> = emptyList(),
         val memos: List<com.qure.ui.model.MemoUI> = emptyList(),
-    ) : HomeUiState {
-        fun toTemperatureString(): String {
-            return "${getTemperatureState().checkedWeather}°"
-        }
-
-        fun toWeatherAnimation(): Int {
-            when {
-                getThunder().fcstValue.toInt() > 1 -> return getThunder().checkedWeather
-                getPrecipitationState().fcstValue.toInt() > 0 -> return getPrecipitationState().checkedWeather
-                else -> return getSkyState().checkedWeather
-            }
-        }
-
-        fun getTemperatureState(): com.qure.ui.model.WeatherUI {
-            return weather.filter { it.category == WeatherCategory.T1H }[0]
-        }
-
-        fun getSkyState(): com.qure.ui.model.WeatherUI {
-            return weather.filter { it.category == WeatherCategory.SKY }[0]
-        }
-
-        fun getPrecipitationState(): com.qure.ui.model.WeatherUI {
-            return weather.filter { it.category == WeatherCategory.PTY }[0]
-        }
-
-        fun getThunder(): com.qure.ui.model.WeatherUI {
-            return weather.filter { it.category == WeatherCategory.LGT }[0]
-        }
-
-        fun toCurrentDateTimeWeather(): String {
-            return "${convertToDate(weather.first().fcstDate)}\n${convertToAmPm(weather.first().fcstTime)} 기준"
-        }
-
-        private fun convertToDate(date: Int): String {
-            val month = (date % 10000) / 100
-            val day = date % 100
-            return String.format("%02d/%02d", month, day)
-        }
-        private fun convertToAmPm(time: Int): String {
-            val hour = time / 100
-            val minute = time % 100
-            val period = if (hour >= 12) "오후" else "오전"
-            val adjustedHour = if (hour % 12 == 0) 12 else hour % 12
-            return String.format("%s %d:%02d", period, adjustedHour, minute)
-        }
-    }
+    ) : HomeUiState
 }
