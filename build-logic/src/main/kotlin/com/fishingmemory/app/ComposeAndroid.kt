c1b91f496f91e3fb36958fab489f11a5cacfc7a0
hegleB <dltkd13956@naver.com>
2024-12-12 19:18:57 +0900

    chore: buildFeatures compose 활성화

--- build-logic/src/main/kotlin/com/fishingmemory/app/ComposeAndroid.kt
+++ build-logic/src/main/kotlin/com/fishingmemory/app/ComposeAndroid.kt
@@ -12,6 +12,10 @@ internal fun Project.configureComposeAndroid() {
 
     val libs = extensions.libs
     androidExtension.apply {
+        buildFeatures {
+            compose = true
+        }
+
         dependencies {
             val bom = libs.findLibrary("androidx-compose-bom").get()
             add("implementation", platform(bom))
@@ -24,7 +28,6 @@ internal fun Project.configureComposeAndroid() {
             add("androidTestImplementation", libs.findLibrary("androidx.test.ext").get())
             add("androidTestImplementation", libs.findLibrary("androidx.test.espresso.core").get())
             add("androidTestImplementation", libs.findLibrary("androidx.compose.ui.test").get())
-            add("debugImplementation", libs.findLibrary("androidx.compose.ui.tooling").get())
             add("debugImplementation", libs.findLibrary("androidx.compose.ui.testManifest").get())
         }
     }
